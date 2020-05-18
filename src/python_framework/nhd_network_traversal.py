bfb2495a252fe02c1dd6525b0eb141b08b7bc509
jhrehanoaa <59839548+jhrehanoaa@users.noreply.github.com>
2020-05-18 14:01:16 -0500

    Argparse (#29)
    * added argparse code bool not working yet
    
    * added command line inputs but cant input mulitple values yet for supernetworks list
     Please enter the commit message for your changes. Lines starting
    
    * fixed input of supernetwork (single)
    
    * clean up booleans
    
    also fixed tabs
    
    Co-authored-by: Jacob Hreha <jacob.hreha@nwcal-ihcb-bm01.nwc.nws.noaa.gov>
    Co-authored-by: James Halgren <james.halgren@noaa.gov>
    Former-commit-id: 76c723f083b206982a19af9fbbbba5d211b8cdc1

--- src/python_framework/nhd_network_traversal.py
+++ src/python_framework/nhd_network_traversal.py
@@ -7,29 +7,56 @@ import nhd_network_utilities as nnu
 import recursive_print
 import time
 import os
+import argparse
 
-# NOTE: these methods can lose the "connections" and "rows" arguments when
-# implemented as class methods where those arrays are members of the class.
+def _handle_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--debuglevel',
+                        help='Set the debuglevel',
+                        dest='debuglevel',
+                        default=0)
+    parser.add_argument('-v, --verbose',
+                        help='Verbose output (leave blank for quiet output)',
+                        dest='verbose',
+                        action='store_true')
+    parser.add_argument('-t, --showtiming',
+                        help='Set the showtiming - leave blank for False',
+                        dest='showtiming',
+                        action='store_true')
+    parser.add_argument('-n, --supernetwork',
+                        help='List of supernetworks (Pocono_TEST1, Pocono_TEST2, LowerColorado_Conchos_FULL_RES, Brazos_LowerColorado_ge5, Brazos_LowerColorado_FULL_RES, Brazos_LowerColorado_Named_Streams, CONUS_ge5, Mainstems_CONUS, CONUS_Named_Streams, CONUS_FULL_RES_v20', 
+                        # TODO: accept multiple or a Path (argparse Action perhaps)
+                        # action='append',
+                        # nargs=1,
+                        dest='supernetwork',
+                        default='Pocono_TEST1')
+
+    return parser.parse_args()
+
 
 def main():
+
+    args = _handle_args()
+
     # find the path of the test scripts, several levels above the script path
     root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     test_folder = os.path.join(root, r'test')
     
-    supernetworks = {}
-    supernetworks.update({'Pocono_TEST1':{}})
-    supernetworks.update({'LowerColorado_Conchos_FULL_RES':{}}) 
-    supernetworks.update({'Brazos_LowerColorado_ge5':{}}) ##NHD Subset (Brazos/Lower Colorado)"""
-    supernetworks.update({'Brazos_LowerColorado_FULL_RES':{}}) 
-    supernetworks.update({'Brazos_LowerColorado_Named_Streams':{}}) 
-    supernetworks.update({'CONUS_ge5':{}}) ##NHD CONUS order 5 and greater"""
-    supernetworks.update({'Mainstems_CONUS':{}})
-    supernetworks.update({'CONUS_Named_Streams':{}})
-    supernetworks.update({'CONUS_FULL_RES_v20':{}}) # = False
+    debuglevel = -1 * int(args.debuglevel)
+    verbose = args.verbose
+    showtiming = args.showtiming
+    supernetworks = {str(args.supernetwork):{}}
 
-    debuglevel = 0
-    verbose = True
-    showtiming = True
+    # supernetworks = {}
+    # supernetworks.update({'Pocono_TEST1':{}})
+    # supernetworks.update({'LowerColorado_Conchos_FULL_RES':{}}) 
+    # supernetworks.update({'Brazos_LowerColorado_ge5':{}}) ##NHD Subset (Brazos/Lower Colorado)"""
+    # supernetworks.update({'Brazos_LowerColorado_FULL_RES':{}}) 
+    # supernetworks.update({'Brazos_LowerColorado_Named_Streams':{}}) 
+    # supernetworks.update({'CONUS_ge5':{}}) ##NHD CONUS order 5 and greater"""
+    # supernetworks.update({'Mainstems_CONUS':{}})
+    # supernetworks.update({'CONUS_Named_Streams':{}})
+    # supernetworks.update({'CONUS_FULL_RES_v20':{}}) # = False
 
     for supernetwork in supernetworks:
         supernetworks[supernetwork] = nnu.set_supernetwork_data(
