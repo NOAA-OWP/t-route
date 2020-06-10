ca2be9b3f64903d63670f3933d6c8eabd21dcf1f
James Halgren <james.halgren@noaa.gov>
2020-06-10 16:38:14 -0500

    force all values to double
    Former-commit-id: dc74b51630293dca5c0b6bcb6831072ee4dd0fd8

--- src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/mc_sseg_stime_NOLOOP_demo.py
+++ src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/mc_sseg_stime_NOLOOP_demo.py
@@ -12,6 +12,7 @@ if COMPILE:
         fortran_compile_call.append(r"MCsingleSegStime_f2py_NOLOOP.f90")
         fortran_compile_call.append(r"-m")
         fortran_compile_call.append(r"mc_sseg_stime")
+        #fortran_compile_call.append(r"--opt='-fdefault-real-8'")
         if debuglevel <= -2:
             subprocess.run(fortran_compile_call)
         else:
@@ -80,8 +81,8 @@ def singlesegment(
 
 def main():
 
-    precision = "double"  # the fortran real*8 declarations must be changed to
-    # precision = 'single # real*4 to use precision single and obtain the expected output below
+    #precision = "double"  # the fortran real*8 declarations must be changed to
+    precision = "single" # real*4 to use precision single and obtain the expected output below
 
     dt = 60.0  # Time step
     dx = 1800.0  # segment length
@@ -127,6 +128,27 @@ def main():
         1 1 0.20140897858530027 0.09322303229159627 0.01503019048616059
         1 2 0.13257014557875424 0.061184620519535104 0.008333345170456535
         1 3 0.7570106991484074 0.12373605347605196 0.02334451636521022
+
+        Xk,   i,   q,    vel,    depth
+        X0 0 0.08917601071480002 0.06941037462636288 0.010050795648053421
+        X0 1 0.06863607847374494 0.07104581370597118 0.009998837063033011
+        X0 2 0.04598825885217007 0.06900733569152935 0.009981763967449955
+        X0 3 0.21487345087737053 0.07048020184743511 0.010033471026476835
+        X1 0 0.3173821233250696 0.15248451094165638 0.03275299244073234
+        X1 1 0.20140900207783252 0.09322304279007165 0.015030191144899728
+        X1 2 0.13257016086743934 0.061184627249756214 0.00833334535644666
+        X1 3 0.7570107902354513 0.12373606306742324 0.02334451646521419
+
+        TEMP
+        k,   i,   q,    vel,    depth
+        0 0 0.08917599589412113 0.06941036720349936 0.010050795413704227
+        0 1 0.06863606840763538 0.07104580614454808 0.009998836839100517
+        0 2 0.04598825231793033 0.06900732835665264 0.009981763746485387
+        0 3 0.21487341391047937 0.07048019432213874 0.01003347079559232
+        1 0 0.3173820852675605 0.15248449837070724 0.03275299172786703
+        1 1 0.20140897858530027 0.09322303229159627 0.01503019048616059
+        1 2 0.13257014557875424 0.061184620519535104 0.008333345170456535
+        1 3 0.7570106991484074 0.12373605347605196 0.02334451636521022
         """
         qup = 0.04598825231793033  # Flow from the upstream neighbor in the previous timestep
         quc = 0.04598825231793033  # Flow from the upstream neighbor in the current timestep
@@ -136,27 +158,28 @@ def main():
         depthp = 0.01003347079559232  # Depth at the current segment in the previous timestep')
         velp = 0.07048019432213874  # Velocity in the current segment in the previous timestep NOT USED AS AN INPUT!!!
 
-    qdc_expected = 0.7570106983184814
-    velc_expected = 0.12373604625463486
-    depthc_expected = 0.02334451675415039
+    qdc_expected_sngl = 0.7570106983184814
+    velc_expected_sngl = 0.12373604625463486
+    depthc_expected_sngl = 0.02334451675415039
 
     qdc_expected_dbl = 0.7570106991484074
     velc_expected_dbl = 0.12373605347605196
     depthc_expected_dbl = 0.02334451636521022
 
     # run M-C model
-    qdc, velc, depthc = singlesegment(
+    qdc, velc, depthc = mc.muskingcungenwm(
+        # precision=precision,
         dt=dt,
         qup=qup,
         quc=quc,
         qdp=qdp,
-        qlat=qlat,
+        ql=qlat,
         dx=dx,
         bw=bw,
         tw=tw,
         twcc=twcc,
-        n_manning=n_manning,
-        n_manning_cc=n_manning_cc,
+        n=n_manning,
+        ncc=n_manning_cc,
         cs=cs,
         s0=s0,
         velp=velp,
@@ -174,7 +197,7 @@ def main():
     )
     print(
         "real single precision expected q: {} vel: {} depth: {}".format(
-            qdc_expected, velc_expected, depthc_expected
+            qdc_expected_sngl, velc_expected_sngl, depthc_expected_sngl
         )
     )
 
--- src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/varSingleSegStime_f2py_2clean.f90
+++ src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/varSingleSegStime_f2py_2clean.f90
@@ -2,15 +2,16 @@ module var
     implicit none
     save
 
-    real*8 :: dt, dx, qup, quc, qdp, qdc, ql
-    real*8 :: Bw, Tw, TwCC, nCC, Cs, So, n, z, vel, depth
-    real*8 :: velp_chk, depthp_chk
-    real*8 :: bfd, WPC, AREAC, C1, C2, C3, C4
+    integer, parameter :: dp = kind(1.d0)
+    real(dp) :: dt, dx, qup, quc, qdp, qdc, ql
+    real(dp) :: Bw, Tw, TwCC, nCC, Cs, So, n, z, vel, depth
+    real(dp) :: velp_chk, depthp_chk
+    real(dp) :: bfd, WPC, AREAC, C1, C2, C3, C4
     integer :: ntim
     integer :: ncomp0, ncomp, iseg, uslinkflag
-    real*8 :: Qus_prev
-    real*8,allocatable,dimension(:,:) :: vela, deptha
-    real*8,allocatable,dimension(:,:) :: Qd
+    real(dp) :: Qus_prev
+    real(dp),allocatable,dimension(:,:) :: vela, deptha
+    real(dp),allocatable,dimension(:,:) :: Qd
 
 
 end module var
