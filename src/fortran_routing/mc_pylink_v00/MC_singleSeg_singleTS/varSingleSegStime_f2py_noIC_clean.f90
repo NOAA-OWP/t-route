a0e004c25f962721305519fa90143147b0f5feed
James Halgren <james.halgren@noaa.gov>
2020-06-12 12:02:18 -0500

    update mc-->muskingcunge_module; bring test notebooks up-to-date
    The test notebooks now take advantage of the precision module to specify the precision of the
    floating point numbers in the fortran modules. As noted, the double precision specification
    is not properly handled by f2py so it is not reliable at this point.

--- src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/varSingleSegStime_f2py_noIC_clean.f90
+++ src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/varSingleSegStime_f2py_noIC_clean.f90
@@ -1,10 +1,10 @@
 module var
+    use precis
     implicit none
     save
 
-    integer, parameter :: dp = kind(1.d0)
-    real(dp) :: dt, dx, qup, quc, qdp, qdc, ql, Bw, Tw, TwCC, nCC, Cs, So, n, z, vel, depth
-    real(dp) :: bfd, WPC, AREAC, C1, C2, C3, C4
+    real(prec) :: dt, dx, qup, quc, qdp, qdc, ql, Bw, Tw, TwCC, nCC, Cs, So, n, z, vel, depth
+    real(prec) :: bfd, WPC, AREAC, C1, C2, C3, C4
 
 end module var
 
