f0d99854ed5c3c69c2a25de4a4a16bf72beef434
James Halgren <james.halgren@noaa.gov>
2020-06-15 09:36:35 -0500

    black python files

--- src/lookup_routing/NN_gen_predicted_values.py
+++ src/lookup_routing/NN_gen_predicted_values.py
@@ -1,9 +1,12 @@
 import numpy as np
-import sys;sys.path.append(r'../fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS')
+import sys
+
+sys.path.append(r"../fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS")
 import mc_sseg_stime_NOLOOP as mc
 import itertools
 import NN_normalization
 import nwm_single_segment
+
 
 def main(
     depthp_min,
@@ -25,33 +28,41 @@ def main(
     bw_min,
     bw_max,
     regr,
-    AL):
-#select the number of points you'd like to sample
+    AL,
+):
+    # select the number of points you'd like to sample
     num_samp = 1000
 
-    dt = 60 # Time step
-    dx = 1800 # segment length
+    dt = 60  # Time step
+    dx = 1800  # segment length
     # bw = np.linspace(0.135, 230.035, array_length, endpoint=True) # Trapezoidal bottom width
-    bw = np.random.uniform(bw_min, bw_max, num_samp) # Trapezoidal bottom width
-    tw = np.random.uniform(tw_min, tw_max, num_samp) # Channel top width (at bankfull)
+    bw = np.random.uniform(bw_min, bw_max, num_samp)  # Trapezoidal bottom width
+    tw = np.random.uniform(tw_min, tw_max, num_samp)  # Channel top width (at bankfull)
     # twcc = np.linspace(0.67, 1150.17, array_length, endpoint=True) # Flood plain width
     # twcc = tw*  # Flood plain width tw*3
-    n_manning = .028   # manning roughness of channel
-    n_manning_cc = .028 # manning roughness of floodplain
-    cs = np.random.uniform(cs_min,cs_max, num_samp)# channel trapezoidal sideslope
-    s0 = np.random.uniform(s0_min, s0_max, num_samp) # Lateral inflow in this time step
-    qup = np.random.uniform(qup_min, qup_max, num_samp) # Flow from the upstream neighbor in the previous timestep
-    # quc = np.linsprandom.uniformace(10, 1000, array_length, endpoint=True) # Flow from the upstream neighbor in the current timestep 
-    quc = np.random.uniform(quc_min, quc_max, num_samp)  # Flow from the upstream neighbor in the current timestep 
+    n_manning = 0.028  # manning roughness of channel
+    n_manning_cc = 0.028  # manning roughness of floodplain
+    cs = np.random.uniform(cs_min, cs_max, num_samp)  # channel trapezoidal sideslope
+    s0 = np.random.uniform(s0_min, s0_max, num_samp)  # Lateral inflow in this time step
+    qup = np.random.uniform(
+        qup_min, qup_max, num_samp
+    )  # Flow from the upstream neighbor in the previous timestep
+    # quc = np.linsprandom.uniformace(10, 1000, array_length, endpoint=True) # Flow from the upstream neighbor in the current timestep
+    quc = np.random.uniform(
+        quc_min, quc_max, num_samp
+    )  # Flow from the upstream neighbor in the current timestep
     # qdp = np.linspace(10, 1000, array_length, endpoint=True) # Flow at the current segment in the previous timestep
-    qdp = np.random.uniform(qdp_min, qdp_max, num_samp)  # Flow at the current segment in the previous timestep
-    qlat = np.random.uniform(qlat_min, qlat_max, num_samp) # lat inflow into current segment in the current timestep
-    velp = .5  # Velocity in the current segment in the previous timestep NOT USED AS AN INPUT!!!
-    depthp = np.random.uniform(depthp_min ,depthp_max , num_samp) # D
+    qdp = np.random.uniform(
+        qdp_min, qdp_max, num_samp
+    )  # Flow at the current segment in the previous timestep
+    qlat = np.random.uniform(
+        qlat_min, qlat_max, num_samp
+    )  # lat inflow into current segment in the current timestep
+    velp = 0.5  # Velocity in the current segment in the previous timestep NOT USED AS AN INPUT!!!
+    depthp = np.random.uniform(depthp_min, depthp_max, num_samp)  # D
 
-
-#you could take real values and insert them here from historical data is you wish (Alex)
-#stepping through and predicting by i will slow this down 
+    # you could take real values and insert them here from historical data is you wish (Alex)
+    # stepping through and predicting by i will slow this down
     rel_errors = []
     errors = np.array([])
     mean_errors_list = []
@@ -60,43 +71,51 @@ def main(
     max_rel_errors_list = []
     for i in range(num_samp):
         temp_y = nwm_single_segment.singlesegment(
-                                    dt=dt,
-                                    qup=qup[i],
-                                    quc=quc[i],
-                                    qlat=qlat[i],
-                                    qdp=qdp[i],
-                                    
-                                    dx=dx ,
-                                    bw=bw[i],
-                                    tw=tw[i],
-                                    twcc=tw[i]*3,
-                                    n_manning=n_manning,
-                                    n_manning_cc=n_manning_cc,
-                                    cs=cs[i],
-                                    s0=s0[i],
-                                    velp=velp,
-                                    depthp=depthp[i])
+            dt=dt,
+            qup=qup[i],
+            quc=quc[i],
+            qlat=qlat[i],
+            qdp=qdp[i],
+            dx=dx,
+            bw=bw[i],
+            tw=tw[i],
+            twcc=tw[i] * 3,
+            n_manning=n_manning,
+            n_manning_cc=n_manning_cc,
+            cs=cs[i],
+            s0=s0[i],
+            velp=velp,
+            depthp=depthp[i],
+        )
 
-        temp_y_interp = regr.predict(np.array( [[NN_normalization.normalize(qup[i],qup_max,qup_min), 
-                                        NN_normalization.normalize(quc[i],quc_max,quc_min), 
-                                        NN_normalization.normalize(qlat[i],qlat_max,qlat_min),
-                                        NN_normalization.normalize(qdp[i],qdp_max,qdp_min),
-                                        # dx,  
-                                        NN_normalization.normalize(bw[i],bw_max,bw_min),
-                                        NN_normalization.normalize(tw[i],tw_max,tw_min),
-                                        # NN_normalization.normalize(tw[tw_o]*3,tw_max,tw_min),
-                                        # n_manning, 
-                                        # n_manning_cc, 
-                                        NN_normalization.normalize(cs[i],cs_max,cs_min),
-                                        NN_normalization.normalize(s0[i], s0_max, s0_min),
-                                        # velp, 
-                                        NN_normalization.normalize(depthp[i],depthp_max,depthp_min)]]))
-#calculates errors
-        if i%1000 == 0:
+        temp_y_interp = regr.predict(
+            np.array(
+                [
+                    [
+                        NN_normalization.normalize(qup[i], qup_max, qup_min),
+                        NN_normalization.normalize(quc[i], quc_max, quc_min),
+                        NN_normalization.normalize(qlat[i], qlat_max, qlat_min),
+                        NN_normalization.normalize(qdp[i], qdp_max, qdp_min),
+                        # dx,
+                        NN_normalization.normalize(bw[i], bw_max, bw_min),
+                        NN_normalization.normalize(tw[i], tw_max, tw_min),
+                        # NN_normalization.normalize(tw[tw_o]*3,tw_max,tw_min),
+                        # n_manning,
+                        # n_manning_cc,
+                        NN_normalization.normalize(cs[i], cs_max, cs_min),
+                        NN_normalization.normalize(s0[i], s0_max, s0_min),
+                        # velp,
+                        NN_normalization.normalize(depthp[i], depthp_max, depthp_min),
+                    ]
+                ]
+            )
+        )
+        # calculates errors
+        if i % 1000 == 0:
             print(i)
         # print(temp_y,temp_y_interp)
         error = abs(temp_y_interp - temp_y[0])
-        rel_error = error/temp_y
+        rel_error = error / temp_y
         rel_errors.append(rel_error)
         # print(error, temp_y_interp, temp_y)
         errors = np.append(errors, error)
@@ -111,12 +130,11 @@ def main(
     mean_rel_errors_list.append(np.mean(rel_errors))
     max_rel_errors_list.append(np.max(rel_errors))
 
-
     print(AL)
-    print("mean errors",mean_errors_list)
-    print("max_errors",max_errors_list)
-    print("mean_rel_errors",mean_rel_errors_list)
-    print("max_rel_errors",max_rel_errors_list)
+    print("mean errors", mean_errors_list)
+    print("max_errors", max_errors_list)
+    print("mean_rel_errors", mean_rel_errors_list)
+    print("max_rel_errors", max_rel_errors_list)
     # print("Last 10 Outputs",temp_y[-10:],temp_y_interp[-10:] )
 
     # num_speed_samp = 2700000
@@ -133,8 +151,8 @@ def main(
     # cs = np.random.uniform(cs_min,cs_max, num_speed_samp)# channel trapezoidal sideslope
     # s0 = np.random.uniform(s0_min, s0_max, num_speed_samp) # Lateral inflow in this time step
     # qup = np.random.uniform(qup_min, qup_max, num_speed_samp) # Flow from the upstream neighbor in the previous timestep
-    # # quc = np.linsprandom.uniformace(10, 1000, array_length, endpoint=True) # Flow from the upstream neighbor in the current timestep 
-    # quc = np.random.uniform(quc_min, quc_max, num_speed_samp)  # Flow from the upstream neighbor in the current timestep 
+    # # quc = np.linsprandom.uniformace(10, 1000, array_length, endpoint=True) # Flow from the upstream neighbor in the current timestep
+    # quc = np.random.uniform(quc_min, quc_max, num_speed_samp)  # Flow from the upstream neighbor in the current timestep
     # # qdp = np.linspace(10, 1000, array_length, endpoint=True) # Flow at the current segment in the previous timestep
     # qdp = np.random.uniform(qdp_min, qdp_max, num_speed_samp)  # Flow at the current segment in the previous timestep
     # qlat = np.random.uniform(qlat_min, qlat_max, num_speed_samp) # lat inflow into current segment in the current timestep
@@ -145,22 +163,22 @@ def main(
 
     # for i in range(num_samp):
     #     speed_x.append( [NN_normalization.normalize(qup[i],qup_max,qup_min),
-    #     NN_normalization.normalize(quc[i],quc_max,quc_min), 
+    #     NN_normalization.normalize(quc[i],quc_max,quc_min),
     #     NN_normalization.normalize(qlat[i],qlat_max,qlat_min),
     #     NN_normalization.normalize(qdp[i],qdp_max,qdp_min),
-    #     # dx,  
+    #     # dx,
     #     NN_normalization.normalize(bw[i],bw_max,bw_min),
     #     NN_normalization.normalize(tw[i],tw_max,tw_min),
     #     # NN_normalization.normalize(tw[tw_o]*3,tw_max,tw_min),
-    #     # n_manning, 
-    #     # n_manning_cc, 
+    #     # n_manning,
+    #     # n_manning_cc,
     #     NN_normalization.normalize(cs[i],cs_max,cs_min),
     #     NN_normalization.normalize(s0[i], s0_max, s0_min),
-    #     # velp, 
+    #     # velp,
     #     NN_normalization.normalize(depthp[i],depthp_max,depthp_min)])
     # speed_x = np.array(speed_x)
 
-    # import time 
+    # import time
     # starttime=time.time()
     # regr.predict(speed_x)
     # print(time.time()-starttime)
