ad961b257ce255d5757909e111e2dffe2913daec
Ryan Grout <ryan@ryangrout.org>
2020-06-22 17:42:23 -0500

    Refactor to release gil for entire computation.

--- src/python_routing_v02/mc_reach.pyx
+++ src/python_routing_v02/mc_reach.pyx
@@ -8,10 +8,12 @@ cimport numpy as np
 cimport cython
 
 
-ctypedef fused float:
-    float
-    double
+cdef struct FDV:
+    float qdc
+    float depthc
+    float velc
 
+@cython.boundscheck(False)
 cpdef object binary_find(object arr, object els):
     """
     Find elements in els in arr.
@@ -43,9 +45,8 @@ cpdef object binary_find(object arr, object els):
         if arr[m] == el:
             idxs.append(m)
         else:
-            break
+            raise ValueError(f"element {el} not found in {np.asarray(arr)}")
     return idxs
-
 
 
 cdef extern from "pyMCsingleSegStime_NoLoop.h":
@@ -68,58 +69,8 @@ cdef extern from "pyMCsingleSegStime_NoLoop.h":
                                   float *velc,
                                   float *depthc) nogil;
 
-cpdef list pymuskingcunge(float dt,
-        float qup,
-        float quc,
-        float qdp,
-        float ql,
-        float dx,
-        float bw,
-        float tw,
-        float twcc,
-        float n,
-        float ncc,
-        float cs,
-        float s0,
-        float velp,
-        float depthp):
-    cdef float qdc, velc, depthc
-    qdc = 0.0
-    velc = 0.0
-    depthc = 0.0
-
-    c_muskingcungenwm(
-        &dt,
-        &qup,
-        &quc,
-        &qdp,
-        &ql,
-        &dx,
-        &bw,
-        &tw,
-        &twcc,
-        &n,
-        &ncc,
-        &cs,
-        &s0,
-        &velp,
-        &depthp,
-        &qdc,
-        &velc,
-        &depthc)
-    return [qdc, depthc, velc]
-    #out[0] = qdc
-    #out[1] = depthc
-    #out[2] = velc
-
-cdef struct FDV:
-    float qdc
-    float depthc
-    float velc
-
 
 @cython.boundscheck(False)
-@cython.wraparound(False)
 cdef void muskingcunge(float dt,
         float qup,
         float quc,
@@ -168,7 +119,6 @@ cdef void muskingcunge(float dt,
 
 
 @cython.boundscheck(False)
-@cython.wraparound(False)
 cdef void compute_reach_kernel(float qup, float quc, int nreach, const float[:,:] input_buf, float[:, :] output_buf) nogil:
     """
     Kernel to compute reach.
@@ -231,7 +181,6 @@ cdef void compute_reach_kernel(float qup, float quc, int nreach, const float[:,:
 
         qup = qdp
 
-
 cdef void fill_buffer_column(const Py_ssize_t[:] srows,
     const Py_ssize_t scol,
     const Py_ssize_t[:] drows,
@@ -241,7 +190,6 @@ cdef void fill_buffer_column(const Py_ssize_t[:] srows,
     cdef Py_ssize_t i
     for i in range(srows.shape[0]):
         out[drows[i], dcol] = src[srows[i], scol]
-
 
 cpdef object column_mapper(object src_cols):
     """Map source columns to columns expected by algorithm"""
@@ -257,7 +205,8 @@ cpdef object column_mapper(object src_cols):
         rv.append(index[label])
     return rv
 
-cpdef object compute_network(int nsteps, object reaches, object connections, 
+
+cpdef object compute_network(int nsteps, list reaches, dict connections, 
     const long[:] data_idx, object[:] data_cols, const float[:,:] data_values, 
     const float[:, :] qlat_values, 
     bint assume_short_ts=False):
@@ -283,65 +232,87 @@ cpdef object compute_network(int nsteps, object reaches, object connections,
         raise ValueError(f"data_values shape mismatch")
 
     cdef float[:,::1] flowveldepth = np.zeros((data_idx.shape[0], nsteps * 3), dtype='float32')
-    cdef int timestep = 0
-    cdef int ts_offset
 
     cdef Py_ssize_t[:] srows, drows_tmp, usrows
     cdef float[:, ::1] buf, buf_view
     cdef float[:, ::1] out_buf, out_view
-    cdef object reach  # list
-    cdef int reach_length
 
     cdef Py_ssize_t[:] scols = np.array(column_mapper(data_cols), dtype=np.intp)
     cdef int buf_cols = 13
-    cdef Py_ssize_t i, ireach
-    cdef float qup, quc
+    cdef Py_ssize_t i, ireach, ireach_cache, iusreach_cache
 
-    cdef int maxreachlen = 0
-    cdef object buf_cache = []
-    for reach in reaches:
-        maxreachlen = max(maxreachlen, len(reach))
-        srows = np.array(binary_find(data_idx, reach), dtype=np.intp)
-        if reach[0] in connections:
-            usrows = np.array(binary_find(data_idx, connections[reach[0]]), dtype=np.intp)
-        else:
-            usrows = np.array()
-        buf_cache.append((srows, usrows))
+    cdef list reach_sizes = list(map(len, reaches))
+    cdef list usreach_sizes = [len(connections.get(reach[0], ())) for reach in reaches]
 
+    cdef list reach, bf_results
+    cdef int reachlen, usreachlen
+    cdef Py_ssize_t bidx
+    cdef list buf_cache = []
+    cdef Py_ssize_t[:] reach_cache = np.empty(sum(reach_sizes) + len(reach_sizes), dtype=np.intp)
+    cdef Py_ssize_t[:] usreach_cache = np.empty(sum(usreach_sizes) + len(usreach_sizes), dtype=np.intp)
+    ireach_cache = 0
+    iusreach_cache = 0
+    # copy reaches into an array
+    for ireach in range(len(reaches)):
+        reachlen = reach_sizes[ireach]
+        usreachlen = usreach_sizes[ireach]
+        reach = reaches[ireach]
+
+        # set the length (must be negative to indicate reach boundary)
+        reach_cache[ireach_cache] = -reachlen
+        ireach_cache += 1
+        bf_results = binary_find(data_idx, reach)
+        for bidx in bf_results:
+            reach_cache[ireach_cache] = bidx
+            ireach_cache += 1
+
+        usreach_cache[iusreach_cache] = -usreachlen
+        iusreach_cache += 1
+        if usreachlen > 0:
+            for bidx in binary_find(data_idx, connections[reach[0]]):
+                usreach_cache[iusreach_cache] = bidx
+                iusreach_cache += 1
+
+    cdef int maxreachlen = max(reach_sizes)
     buf = np.empty((maxreachlen, buf_cols), dtype='float32')
     out_buf = np.empty((maxreachlen, 3), dtype='float32')
 
     drows_tmp = np.arange(maxreachlen, dtype=np.intp)
     cdef Py_ssize_t[:] drows
-    while timestep < nsteps:
-        ts_offset = timestep * 3
+    cdef float qup, quc
+    cdef int timestep = 0
+    cdef int ts_offset
 
-        #print(f"Timestep: {timestep}".ljust(120, '='))
-        for ireach in range(len(reaches)):
-            reach = reaches[ireach]
-            srows = buf_cache[ireach][0]
-            usrows = buf_cache[ireach][1]
-            
-            with nogil:
-                # compute qup/quc
+    with nogil:
+        while timestep < nsteps:
+            ts_offset = timestep * 3
+
+            ireach_cache = 0
+            iusreach_cache = 0
+            while ireach_cache < reach_cache.shape[0]:
+                reachlen = -reach_cache[ireach_cache]
+                usreachlen = -usreach_cache[iusreach_cache]
+
+                ireach_cache += 1
+                iusreach_cache += 1
+                #print(ireach_cache, iusreach_cache, np.asarray(reach_cache, dtype=np.intp), np.asarray(usreach_cache, dtype=np.intp))
+
                 qup = 0.0
                 quc = 0.0
-                for i in range(usrows.shape[0]):
-                    i = usrows[i]
-                    quc += flowveldepth[i, ts_offset]
+                for i in range(usreachlen):
+                    quc += flowveldepth[usreach_cache[iusreach_cache + i], ts_offset]
                     if timestep > 0:
-                        qup += flowveldepth[i, ts_offset - 3]
+                        qup += flowveldepth[usreach_cache[iusreach_cache + i], ts_offset - 3]
 
-                reach_length = srows.shape[0]
-                buf_view = buf[:reach_length, :]
-                out_view = out_buf[:reach_length, :]
-                drows = drows_tmp[:reach_length]
+                buf_view = buf[:reachlen, :]
+                out_view = out_buf[:reachlen, :]
+                drows = drows_tmp[:reachlen]
+                srows = reach_cache[ireach_cache:ireach_cache+reachlen]
 
-                # fill the buffer with qlat
                 fill_buffer_column(srows, timestep, drows, 0, qlat_values, buf_view)
                 for i in range(scols.shape[0]):
-                    fill_buffer_column(srows, scols[i], drows, i + 1, data_values, buf_view)
-                # fill buffer with qdp, depthp, velp
+                        fill_buffer_column(srows, scols[i], drows, i + 1, data_values, buf_view)
+                    # fill buffer with qdp, depthp, velp
                 if timestep > 0:
                     fill_buffer_column(srows, ts_offset - 3, drows, 10, flowveldepth, buf_view)
                     fill_buffer_column(srows, ts_offset - 2, drows, 11, flowveldepth, buf_view)
@@ -356,11 +327,15 @@ cpdef object compute_network(int nsteps, object reaches, object connections,
                 if assume_short_ts:
                     quc = qup
 
-                compute_reach_kernel(qup, quc, reach_length, buf_view, out_view)
+                compute_reach_kernel(qup, quc, reachlen, buf_view, out_view)
 
                 # copy out_buf results back to flowdepthvel
                 for i in range(3):
                     fill_buffer_column(drows, i, srows, ts_offset + i, out_view, flowveldepth)
-            
-        timestep += 1
+
+                # Update indexes to point to next reach
+                ireach_cache += reachlen
+                iusreach_cache += usreachlen
+                
+            timestep += 1
     return np.asarray(data_idx, dtype=np.intp), np.asarray(flowveldepth, dtype='float32')
