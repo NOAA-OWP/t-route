cfc8eb6e17282b2a20404f34dbfc02181feac2c1
James Halgren <james.halgren@noaa.gov>
2020-06-23 17:48:55 -0500

    set debuglevel/verbosity to maximum for test

--- src/python_framework/networkbuilder.py
+++ src/python_framework/networkbuilder.py
@@ -376,6 +376,8 @@ def main():
     print("")
     print("Executing Test")
     # Test data
+    debuglevel = -3
+    verbose = True
     test_rows = [
         [50, 178, 51, 0],
         [51, 178, 50, 0],
@@ -435,8 +437,8 @@ def main():
         mask_set={row[test_key_col] for row in test_rows},
         downstream_col=test_downstream_col,
         length_col=test_length_col,
-        verbose=True,
-        debuglevel=-2,
+        verbose=verbose,
+        debuglevel=debuglevel,
     )
 
     (
@@ -449,8 +451,8 @@ def main():
     ) = determine_keys(
         connections=test_connections,
         terminal_code=test_terminal_code,
-        verbose=True,
-        debuglevel=-2,
+        verbose=verbose,
+        debuglevel=debuglevel,
     )
 
     (
@@ -464,8 +466,8 @@ def main():
         terminal_code=test_terminal_code,
         headwater_keys=test_headwater_keys,
         terminal_keys=test_terminal_keys,
-        verbose=True,
-        debuglevel=-2,
+        verbose=verbose,
+        debuglevel=debuglevel,
     )
 
     # TODO: Set/pass/identify a proper flag value
@@ -480,8 +482,8 @@ def main():
             terminal_code=test_terminal_code,
             waterbody_col=test_waterbody_col,
             waterbody_null_code=test_waterbody_null_code,
-            verbose=True,
-            debuglevel=-2,
+            verbose=verbose,
+            debuglevel=debuglevel,
         )
 
     recursive_print.print_connections(
@@ -491,7 +493,7 @@ def main():
         terminal_code=test_terminal_code,
         terminal_keys=test_terminal_keys,
         terminal_ref_keys=test_terminal_ref_keys,
-        debuglevel=-2,
+        debuglevel=debuglevel,
     )
 
     recursive_print.print_basic_network_info(
@@ -500,8 +502,8 @@ def main():
         junction_keys=test_junction_keys,
         terminal_keys=test_terminal_keys,
         terminal_code=test_terminal_code,
-        verbose=True,
-        debuglevel=-2,
+        verbose=verbose,
+        debuglevel=debuglevel,
     )
 
 
