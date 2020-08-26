9c4d0bda3f958dc287317da8109d544434d2a182
James Halgren <james.halgren@noaa.gov>
2020-08-26 14:57:34 -0500

    multiply by dt for correct qlat accumulation

--- src/python_routing_v01/compute_nhd_routing_SingleSeg.py
+++ src/python_routing_v01/compute_nhd_routing_SingleSeg.py
@@ -597,7 +597,7 @@ def compute_mc_reach_up2down(
         # storage
         volumec = dt * (quc - qdc + qlat)
         # TODO: This qlatCum is invalid as a cumulative value unless time is factored in
-        qlatCum = qlat
+        qlatCum = qlat * dt
         if ts > 0:
             volumec = volumec + flowveldepth[current_segment]["storageval"][-1]
             qlatCum = qlatCum + flowveldepth[current_segment]["qlatCumval"][-1]
@@ -743,7 +743,7 @@ def compute_level_pool_reach_up2down(
 
     volumec = dt * (quc - qdc + qlat)
     # TODO: This qlatCum is invalid as a cumulative value unless time is factored in
-    qlatCum = qlat
+    qlatCum = qlat * dt
     if ts > 0:
         volumec = volumec + flowveldepth[current_segment]["storageval"][-1]
         qlatCum = qlatCum + flowveldepth[current_segment]["qlatCumval"][-1]
