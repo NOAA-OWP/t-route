2451f9a9af48014a0c2dce24a9111627ac7cc5f0
James Halgren <james.halgren@noaa.gov>
2020-09-30 11:45:29 -0500

    fix floating point segment ids

--- src/python_framework_v02/nhd_io.py
+++ src/python_framework_v02/nhd_io.py
@@ -66,6 +66,7 @@ def replace_downstreams(data, downstream_col, terminal_code):
 
     # Also set negative any nodes in downstream col not in data.index
     new_data.loc[~data[downstream_col].isin(data.index), downstream_col] *= -1
+    new_data[downstream_col] = new_data[downstream_col].apply(int)
     return new_data
 
 
