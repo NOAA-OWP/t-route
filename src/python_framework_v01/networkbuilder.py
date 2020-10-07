c9aa81466602f024cc75649bfa84dc252cd1d08f
jhrehanoaa <jacob.hreha@noaa.gov>
2020-10-07 09:09:47 -0500

    slight change

--- src/python_framework_v01/networkbuilder.py
+++ src/python_framework_v01/networkbuilder.py
@@ -3,33 +3,6 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
-LOG = logging.getLogger("log")
-if verbose:
-    LOG.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-elif debuglevel == 1:
-    LOG.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-elif debuglevel == 2:
-    LOG.setLevel(logging.WARNING)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.WARNING)
-else:
-    LOG.setLevel(logging.CRITICAL)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.CRITICAL)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-LOG.addHandler(ch)
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%m/%d/%Y %I:%M:%S %p",
-    filename="logs.log",
-    filemode=w,
-)
-
 def get_down_connections(
     rows,
     key_col,
