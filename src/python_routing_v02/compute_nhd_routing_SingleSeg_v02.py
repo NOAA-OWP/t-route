f28c1c84e113baf6bc63d3af42b2b32b135e30db
James Halgren <james.halgren@noaa.gov>
2020-10-16 11:01:47 -0500

    remove merge artifact

--- src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
+++ src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
@@ -103,6 +103,7 @@ if ENV_IS_CL:
 elif not ENV_IS_CL:
     root = pathlib.Path("../..").resolve()
     sys.path.append(r"../python_framework_v02")
+
     # TODO: automate compile for the package scripts
     sys.path.append("fast_reach")
 
