e5cfc175b55640f6427be8a8cc0f8410527d6ddb
jhrehanoaa <jacob.hreha@noaa.gov>
2020-10-19 11:08:29 -0500

    working comparison between two PR -v and all debuglevels

--- src/python_routing_v01/compare_files.py
+++ src/python_routing_v01/compare_files.py
@@ -14,7 +14,7 @@ def _handle_args():
         "--root_folder_PR1",
         help="Choose the folder of the top most root location of your first PR request comparison.",
         dest="root_folder_PR1",
-        default="Github",
+        default="github",
     )
     parser.add_argument(
         "-n",
@@ -46,60 +46,64 @@ sys.path.append(fortran_routing_dir)
 routing_directory = os.path.join(root, root_folder_PR2, r"t-route", r"src", r"python_routing_v01")
 routing_directory_v02 = os.path.join(root, root_folder_PR2, r"t-route", r"src", r"python_routing_v02")
 
-debuglevel = 0
-COMPILE = True
-if COMPILE:
-    try:
-        import subprocess
-        from subprocess import PIPE, run
+for x in range(0,4):
+    os.chdir(routing_directory)
+    debuglevel = x
+    COMPILE = True
+    if COMPILE:
+        try:
+            import subprocess
+            from subprocess import PIPE, run
+            python_compile_call = []
+            python_compile_call.append(r"python3")
+            python_compile_call.append(r"compute_nhd_routing_SingleSeg.py")
+            python_compile_call.append(r"--verbose")
+            python_compile_call.append(r'--debuglevel '+ x)
 
-        python_compile_call = []
-        python_compile_call.append(r"python3")
-        python_compile_call.append(r"compute_nhd_routing_SingleSeg.py")
-        python_compile_call.append(r"--verbose")
-
-    except Exception as e:
-        print(e)
-        if debuglevel <= -1:
-            traceback.print_exc()
-
-
-result_sseg1 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-print(result_sseg1.returncode, result_sseg1.stdout, result_sseg1.stderr)
-# print(root)
-# print(sys.path)
-os.chdir(routing_directory_v02)
-debuglevel = 0
-COMPILE = True
-if COMPILE:
-    try:
-        import subprocess
-        from subprocess import PIPE, run
-
-        python_compile_call = []
-        python_compile_call.append(r"python3")
-        python_compile_call.append(r"compute_nhd_routing_SingleSeg_v02.py")
-        python_compile_call.append(r"--verbose")
-
-    except Exception as e:
-        print(e)
-        if debuglevel <= -1:
-            traceback.print_exc()
+        except Exception as e:
+            # print(e)
+            if debuglevel <= -4:
+                traceback.print_exc()
 
 
-result_sseg_v02_1 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-print(result_sseg_v02_1.returncode, result_sseg_v02_1.stdout, result_sseg_v02_1.stderr)
-# print(sys.path)
-result_sseg2, result_sseg_v02_2 = cfu.pr_check(root_folder_PR2)
+    result_sseg1 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    # print(result_sseg1.returncode, result_sseg1.stdout, result_sseg1.stderr)
+    result_sseg1 = str(subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True))
+    # print(root)
+    # print(sys.path)
+    os.chdir(routing_directory_v02)
+    debuglevel = x
+    COMPILE = True
+    if COMPILE:
+        try:
+            import subprocess
+            from subprocess import PIPE, run
 
-if result_sseg1 == result_sseg2:
-    print("PASS - results of compute_nhd_routing_SingleSeg.py are equal")
-else:
-    print("FAIL - results of compute_nhd_routing_SingleSeg.py are not equivalent")
+            python_compile_call = []
+            python_compile_call.append(r"python3")
+            python_compile_call.append(r"compute_nhd_routing_SingleSeg_v02.py")
+            python_compile_call.append(r"--verbose")
+            python_compile_call.append(r'--debuglevel '+ -x)
+        except Exception as e:
+            # print(e)
+            if debuglevel <= -4:
+                traceback.print_exc()
 
-if result_sseg_v02_1 == result_sseg_v02_2:
-    print("PASS - results of compute_nhd_routing_SingleSeg_v02.py are equal")
-else:
-    print("FAIL - results of compute_nhd_routing_SingleSeg_v02.py are not equivalent")
+
+    result_sseg_v02_1 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    # print(result_sseg_v02_1.returncode, result_sseg_v02_1.stdout, result_sseg_v02_1.stderr)
+    result_sseg_v02_1 = str(subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True))
+    # print(sys.path)
+    result_sseg2, result_sseg_v02_2 = cfu.pr_check(root_folder_PR2,x)
+
+    if result_sseg1 == result_sseg2:
+        print("PASS - results of compute_nhd_routing_SingleSeg.py match : debuglevel " + str(x) )
+    else:
+        print("FAIL - results of compute_nhd_routing_SingleSeg.py are not equivalent : debuglevel " + str(x) )
+
+    if result_sseg_v02_1 == result_sseg_v02_2:
+        print("PASS - results of compute_nhd_routing_SingleSeg_v02.py match: debuglevel " + str(x) )
+    else:
+        print("FAIL - results of compute_nhd_routing_SingleSeg_v02.py are not equivalent : debuglevel " + str(x) )
 
 
--- src/python_routing_v01/compare_files_utility.py
+++ src/python_routing_v01/compare_files_utility.py
@@ -3,7 +3,7 @@ import os
 import sys 
 import argparse
 
-def pr_check(root_folder_PR2):
+def pr_check(root_folder_PR2,x):
     root = pathlib.Path("../../../../").resolve()
     sys.setrecursionlimit(4000)
     sys.path.append(os.path.join(root, root_folder_PR2, r"t-route", r"src", r"python_framework_v01"))
@@ -20,8 +20,8 @@ def pr_check(root_folder_PR2):
     sys.path.append(fortran_routing_dir)
 
     os.chdir(routing_directory)
-
-    debuglevel = 0
+    
+    debuglevel = x
     COMPILE = True
     if COMPILE:
         try:
@@ -32,18 +32,18 @@ def pr_check(root_folder_PR2):
             python_compile_call.append(r"python3")
             python_compile_call.append(r"compute_nhd_routing_SingleSeg.py")
             python_compile_call.append(r"--verbose")
-
+            python_compile_call.append(r'--debuglevel '+ x)
         except Exception as e:
-            print(e)
-            if debuglevel <= -1:
+            # print(e)
+            if debuglevel <= -4:
                 traceback.print_exc()
 
     result_sseg2 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(result_sseg2.returncode, result_sseg2.stdout, result_sseg2.stderr)
-
+    # print(result_sseg2.returncode, result_sseg2.stdout, result_sseg2.stderr)
+    result_sseg2 = str(subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True))
     os.chdir(routing_directory_v02)
 
-    debuglevel = 0
+    debuglevel = x
     COMPILE = True
     if COMPILE:
         try:
@@ -54,15 +54,16 @@ def pr_check(root_folder_PR2):
             python_compile_call.append(r"python3")
             python_compile_call.append(r"compute_nhd_routing_SingleSeg_v02.py")
             python_compile_call.append(r"--verbose")
-
+            python_compile_call.append(r'--debuglevel '+ -x)
         except Exception as e:
-            print(e)
-            if debuglevel <= -1:
+            # print(e)
+            if debuglevel <= -4:
                 traceback.print_exc()
 
 
     result_sseg_v02_2 = subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(result_sseg_v02_2.returncode, result_sseg_v02_2.stdout, result_sseg_v02_2.stderr)
+    # print(result_sseg_v02_2.returncode, result_sseg_v02_2.stdout, result_sseg_v02_2.stderr)
+    result_sseg_v02_2 = str(subprocess.run(python_compile_call, stdout=PIPE, stderr=PIPE, universal_newlines=True))
     # print(root)
     # print(sys.path)
     return result_sseg2, result_sseg_v02_2
