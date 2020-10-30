978c4d284a8fb019119e862a02166a01deafc0d6
Nels Frazier <nels.frazier@noaa.gov>
2020-10-30 14:32:14 -0500

    add setup.py to build/install fast_reach extensions

---
+++ src/python_routing_v02/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import sys
+import numpy as np
+
+"""
+If source ships with the cython generated .c code, then cython isn't a hard requirement
+This setup can use if if passed the --use-cython flag, otherwise it looks for the c source
+and builds using distutils.
+"""
+
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
+
+ext = 'pyx' if USE_CYTHON else 'c'
+
+reach = Extension("reach",
+        sources = ["fast_reach/reach.{}".format(ext)],
+        extra_objects = ['fast_reach/mc_single_seg.o', 'fast_reach/pymc_single_seg.o' ])
+
+mc_reach = Extension("mc_reach",
+          sources = ["fast_reach/mc_reach.{}".format(ext)],
+          include_dirs = [np.get_include()],
+          libraries=[],
+          library_dirs=[],
+          extra_objects=[])
+
+ext_modules=[
+    reach, mc_reach
+    ]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    ext_modules = cythonize(ext_modules)
+
+setup(
+  name = 'compute_network_mc',
+  ext_modules = ext_modules,
+)
