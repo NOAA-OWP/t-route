0fdf7569ee2c5902b90b79ed6f61e80059f5ba32
Nels Frazier <nels.frazier@noaa.gov>
2020-11-05 09:04:13 -0600

    setup for distributing troute and troute.network package

---
+++ src/python_framework_v02/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import sys
+import numpy as np
+"""
+If source ships with the cython generated .c code, then cython isn't a hard requirement
+This setup can use if if passed the --use-cython flag, otherwise it looks for the c source
+and builds using distutils.
+"""
+
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
+
+ext = 'pyx' if USE_CYTHON else 'c'
+
+reach = Extension("troute.network.reach",
+        sources = ["troute/network/reach.{}".format(ext)],
+        include_dirs=[np.get_include()],
+        extra_objects = [])
+
+package_data = {
+    'troute.network':['reach.pxd']
+    }
+ext_modules=[
+    reach
+    ]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    ext_modules = cythonize(ext_modules)
+
+setup(
+  name = 'Network',
+  packages=['troute', 'troute.network'],
+  ext_modules = ext_modules,
+  package_data = package_data,
+)
