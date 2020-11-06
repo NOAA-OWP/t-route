2d094822bc8d636e8a2bad38d636bb68d8640e0c
Nels Frazier <nels.frazier@noaa.gov>
2020-11-06 17:50:40 -0600

    extending extension types to have explictily backed C structs

--- src/python_framework_v02/troute/network/reach.pyx
+++ src/python_framework_v02/troute/network/reach.pyx
@@ -69,8 +69,42 @@ cdef class MC_Reach():
   #cdef readonly np.ndarray upstream_ids
 
   def __init__(self, segments, long[::1] upstream_ids):
+    self._num_segments = len(segments)
+    self._num_upstream_ids = len(upstream_ids)
+
+    self._segments = <_MC_Segment*> malloc(sizeof(_MC_Segment)*(self._num_segments))
+    for i, segment in enumerate(segments):
+      self._segments[i].id = segment.id
+      self._segments[i].dt = segment.dt
+      self._segments[i].dx = segment.dx
+      self._segments[i].bw = segment.bw
+      self._segments[i].tw = segment.tw
+      self._segments[i].twcc = segment.twcc
+      self._segments[i].n = segment.n
+      self._segments[i].ncc = segment.ncc
+      self._segments[i].cs = segment.cs
+      self._segments[i].s0 = segment.s0
+      self._segments[i].qdp = segment.qdp
+      self._segments[i].velp = segment.velp
+      self._segments[i].depthp = segment.depthp
+
     self.segments = segments
     self.upstream_ids = np.asarray(upstream_ids)
+    #init the C struct
+    self._reach._segments = self._segments
+    self._reach._num_segments = self._num_segments
+    self.num_segments = self._num_segments
+    self._reach._upstream_ids = <long*>malloc(sizeof(long)*self._num_upstream_ids)
+    self._reach._num_upstream_ids = self._num_upstream_ids
+    for i in range(self._num_upstream_ids):
+      self._reach._upstream_ids[i] = upstream_ids[i]
+
+  def __dealloc__(self):
+    """
+
+    """
+    free(self._segments)
+    free(self._reach._upstream_ids)
 
   cdef route(self):
     """
