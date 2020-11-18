4813d7968c340b3b5e7b5a7dd9957436acbe0646
Donald W Johnson <Donald.W.Johnson@noaa.gov>
2020-11-18 13:27:51 -0600

    Debuging v1.

--- src/external_connections/next_gen_network.py
+++ src/external_connections/next_gen_network.py
@@ -14,6 +14,7 @@ import pandas as pd
 from functools import partial
 from itertools import chain, islice
 import next_gen_io
+import json
 
 def _handle_args():
     parser = argparse.ArgumentParser(
@@ -29,7 +30,7 @@ def _handle_args():
 
     return parser.parse_args()
 
-root = pathlib.Path('../..').resolve()
+root = pathlib.Path('.').resolve()
 sys.path.append(r"../python_framework_v02")
 sys.path.append(r"../python_routing_v02")
 sys.path.append(r"../../test/input/next_gen")
@@ -45,33 +46,34 @@ def main():
 
     args = _handle_args()
 
-    #The following 2 values are currently hard coded for this test domain
+    # The following 2 values are currently hard coded for this test domain
     nts = 720  # number of timestep = 1140 * 60(model timestep) = 86400 = day
     dt_mc = 300.0  # time interval for MC
 
-    #Currently tested on the Sugar Creek domain
+    # Currently tested on the Sugar Creek domain
     ngen_network_df = nhd_io.read_geopandas(os.path.join(next_gen_input_folder, args.supernetwork))
 
-    #Create dictionary mapping each connection ID
-    ngen_network_dict = dict(zip(ngen_network_df.ID, ngen_network_df.toID))
+    # Create dictionary mapping each connection ID
+    ngen_network_dict = dict(zip(ngen_network_df.id, ngen_network_df.toid))
+    #ngen_network_dict = dict(zip(ngen_network_df.ID, ngen_network_df.toID))
 
     def node_key_func(x):
-        return int(x[4:])
+        return int(x[3:])
 
-    #Extract the ID integer values
+    # Extract the ID integer values
     waterbody_connections = {node_key_func(k): node_key_func(v) for k, v in ngen_network_dict.items()}
 
-    #Convert dictionary connections to data frame and make ID column the index
+    # Convert dictionary connections to data frame and make ID column the index
     waterbody_df = pd.DataFrame.from_dict(waterbody_connections, orient='index', columns=['to'])
 
-    #Sort ID index column
+    # Sort ID index column
     waterbody_df = waterbody_df.sort_index()
 
     waterbody_df = nhd_io.replace_downstreams(waterbody_df, "to", 0)
 
     connections = nhd_network.extract_connections(waterbody_df, "to")
 
-    #Read and convert catchment lateral flows to format that can be processed by compute_network
+    # Read and convert catchment lateral flows to format that can be processed by compute_network
     qlats = next_gen_io.read_catchment_lateral_flows(next_gen_input_folder)
 
     rconn = nhd_network.reverse_network(connections)
@@ -80,7 +82,13 @@ def main():
 
     waterbody_df['dt'] = 300.0
 
-    #Setting all below to 1.0 until we can get the appropriate parameters
+    # read the routelink file
+    # nhd_routelink = nhd_io.read_netcdf("data/RouteLink_NHDPLUS.nc")
+
+    # with open("data/default/crosswalk.json") as f:
+    #    data = json.load(f)
+
+    # Setting all below to 1.0 until we can get the appropriate parameters
     waterbody_df['bw'] = 1.0
     waterbody_df['tw'] = 1.0
     waterbody_df['twcc'] = 1.0
@@ -89,6 +97,12 @@ def main():
     waterbody_df['ncc'] = 1.0
     waterbody_df['cs'] = 1.0
     waterbody_df['s0'] = 1.0
+
+    # initial conditions, assume to be zero
+    # TO DO: Allow optional reading of initial conditions from WRF
+    q0 = pd.DataFrame(
+        0, index=waterbody_df.index, columns=["qu0", "qd0", "h0"], dtype="float32"
+    )
 
     #Set types as float32
     waterbody_df = waterbody_df.astype({"dt": "float32", "bw": "float32", "tw": "float32", "twcc": "float32", "dx": "float32", "n": "float32", "ncc": "float32", "cs": "float32", "s0": "float32"})
@@ -105,8 +119,9 @@ def main():
         r = list(chain.from_iterable(reach))
         data_sub = waterbody_df.loc[r, ['dt', 'bw', 'tw', 'twcc', 'dx', 'n', 'ncc', 'cs', 's0']].sort_index()
         qlat_sub = qlats.loc[r].sort_index()
+        q0_sub = q0.loc[r].sort_index()
         results.append(mc_reach.compute_network(
-            nts, reach, subnets[tw], data_sub.index.values, data_sub.columns.values, data_sub.values, qlat_sub.values
+            nts, reach, subnets[tw], data_sub.index.values, data_sub.columns.values, data_sub.values, qlat_sub.values, q0_sub.values
             )
         )
 
