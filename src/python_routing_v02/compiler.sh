eb0b0e0acf15cc4ebca862381e71fcdd90195337
Jacob Hreha <jacob.hreha@noaa.gov>
2020-11-24 11:13:20 -0600

    compilation file and safe load yaml

---
+++ src/python_routing_v02/compiler.sh
@@ -0,0 +1,21 @@
+#TODO add options for clean/noclean, make/nomake, cython/nocython
+#TODO include instuctions on blowing away entire package for fresh install e.g. rm -r ~/venvs/mesh/lib/python3.6/site-packages/troute/*
+#set root folder of github repo (should be named t-route)
+cd ../../
+REPOROOT=`pwd`
+
+#building reach and resevoir kernel files .o
+cd $REPOROOT/src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/
+make clean
+make
+make install
+
+#creates troute package
+cd $REPOROOT/src/python_framework_v02
+rm -rf build
+python setup.py --use-cython install
+
+#updates troute package with the execution script
+cd $REPOROOT/src/python_routing_v02
+rm -rf build
+python setup.py --use-cython install
