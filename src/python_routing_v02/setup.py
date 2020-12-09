78c4cb408e09db0f1fb43455319195a13ac6cf14
Ryan Grout <ryan@ryangrout.org>
2020-12-08 21:45:43 -0600

    Use setuptools.setup

--- src/python_routing_v02/setup.py
+++ src/python_routing_v02/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 import sys
 import numpy as np
@@ -19,14 +19,16 @@ ext = 'pyx' if USE_CYTHON else 'c'
 
 reach = Extension("reach",
         sources = ["fast_reach/reach.{}".format(ext)],
-        extra_objects = ['fast_reach/mc_single_seg.o', 'fast_reach/pymc_single_seg.o' ])
+        extra_objects = ['fast_reach/mc_single_seg.o', 'fast_reach/pymc_single_seg.o' ],
+        extra_compile_args=['-g'])
 
 mc_reach = Extension("mc_reach",
           sources = ["fast_reach/mc_reach.{}".format(ext)],
           include_dirs = [np.get_include()],
           libraries=[],
           library_dirs=[],
-          extra_objects=[])
+          extra_objects=[],
+          extra_compile_args=['-g'])
 
 ext_modules=[
     reach, mc_reach
@@ -34,7 +36,7 @@ ext_modules=[
 
 if USE_CYTHON:
     from Cython.Build import cythonize
-    ext_modules = cythonize(ext_modules)
+    ext_modules = cythonize(ext_modules, compiler_directives={'language_level': 3})
 
 setup(
   name = 'compute_network_mc',
