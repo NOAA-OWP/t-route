68ab3422587dcadc2b3397bbcc8587911be31d4d
James Halgren <james.halgren@noaa.gov>
2020-12-11 10:49:57 -0600

    Add new network options to main compute script
    The following basins were added as options to the v02 nhd_network_utilities script as
    part of PR #177, introducing ability to prepared modified networks. In addition to the
    new network-modification script, these networks are now in the main compute script:
    +            "CapeFear_FULL_RES",
    +            "Florence_FULL_RES"

--- src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
+++ src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
@@ -116,6 +116,8 @@ def _handle_args():
             "Mainstems_CONUS",
             "CONUS_Named_Streams",
             "CONUS_FULL_RES_v20",
+            "CapeFear_FULL_RES",
+            "Florence_FULL_RES"
         ],
         # TODO: accept multiple or a Path (argparse Action perhaps)
         # action='append',
