b0d9fc11621463ebd33d022371d8a8f15951c7cd
James Halgren <james.halgren@noaa.gov>
2020-12-18 21:32:58 +0000

    fix Bug in disjoint check for unbranched networks

--- src/python_framework_v02/troute/nhd_network.py
+++ src/python_framework_v02/troute/nhd_network.py
@@ -153,7 +153,11 @@ def reachable_network(N, sources=None, targets=None, check_disjoint=True):
 
     """
     reached = reachable(N, sources=sources, targets=targets)
-    if check_disjoint and reduce(set.intersection, reached.values()):
+    if (
+        check_disjoint
+        and len(reached) > 1
+        and reduce(set.intersection, reached.values())
+    ):
         raise ValueError("Networks not disjoint")
 
     rv = {}
