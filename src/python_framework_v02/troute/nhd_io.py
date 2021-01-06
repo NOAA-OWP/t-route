01fd549b4605c6be489c72cb4cf2d87c1920ac67
Jacob Hreha <jacob.hreha@noaa.gov>
2021-01-06 09:05:13 -0600

    added context manager for ds

--- src/python_framework_v02/troute/nhd_io.py
+++ src/python_framework_v02/troute/nhd_io.py
@@ -235,25 +235,25 @@ def get_ql_from_wrf_hydro(qlat_files, index_col="station_id", value_col="q_later
 
 def get_usgs_from_wrf_hydro(routelink_file,usgs_timeslices_folder,file_name):
 
-    ds = xr.open_dataset("../../test/input/geo/routelink/routeLink_subset.nc")
-    df = ds.to_dataframe()
-    df = df.reset_index()
-    df.drop(df.columns.difference(['link','to','gages','ascendingIndex']), 1, inplace=True)
-    df['gages'] = df['gages'].str.decode('utf-8')
-    nan_value = float("NaN")
-    df['gages'].replace('               ', nan_value, inplace=True)
-    df = df[df['gages'].notna()]
-    df.dropna(subset=['gages'], inplace=True)
-    df = df.set_index('gages')
+    with xr.open_dataset("../../test/input/geo/routelink/routeLink_subset.nc") as ds:
+        df = ds.to_dataframe()
+        df = df.reset_index()
+        df.drop(df.columns.difference(['link','to','gages','ascendingIndex']), 1, inplace=True)
+        df['gages'] = df['gages'].str.decode('utf-8')
+        nan_value = float("NaN")
+        df['gages'].replace('               ', nan_value, inplace=True)
+        df = df[df['gages'].notna()]
+        df.dropna(subset=['gages'], inplace=True)
+        df = df.set_index('gages')
     
-    ds = xr.open_dataset(usgs_timeslices_folder + file_name)
-    df2 = ds.to_dataframe()
-    df2['stationId'] = df2['stationId'].str.decode('utf-8') 
-    df2['time'] = df2['time'].str.decode('utf-8') 
-    df2 = df2.reset_index()
-    df2.drop(df2.columns.difference(['stationId','time','discharge','discharge_quality']), 1, inplace=True)
-    df2.reset_index()
-    df2.set_index('stationId', inplace=True)
+    with xr.open_dataset(usgs_timeslices_folder + file_name) as ds:
+        df2 = ds.to_dataframe()
+        df2['stationId'] = df2['stationId'].str.decode('utf-8') 
+        df2['time'] = df2['time'].str.decode('utf-8') 
+        df2 = df2.reset_index()
+        df2.drop(df2.columns.difference(['stationId','time','discharge','discharge_quality']), 1, inplace=True)
+        df2.reset_index()
+        df2.set_index('stationId', inplace=True)
 
     usgs_df = df2.join(df)
     usgs_df = usgs_df.reset_index()
