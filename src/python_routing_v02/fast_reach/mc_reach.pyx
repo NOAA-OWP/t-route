6218b1cc2be97f485b56588bd2a7f2a1f4bd967c
James Halgren <james.halgren@noaa.gov>
2021-02-11 13:27:52 -0600

    use int instead of bint

--- src/python_routing_v02/fast_reach/mc_reach.pyx
+++ src/python_routing_v02/fast_reach/mc_reach.pyx
@@ -187,7 +187,7 @@ cpdef object compute_network(
     # rows: indexed by data_idx
     cdef float[:,::1] flowveldepth = np.zeros((data_idx.shape[0], nsteps * 3), dtype='float32')
 
-    cdef bint gages_size = len(usgs_positions_list)
+    cdef int gages_size = len(usgs_positions_list)
     cdef int gage_i, usgs_position_i
 
     # Pseudocode: LOOP ON Upstream Inflowers
