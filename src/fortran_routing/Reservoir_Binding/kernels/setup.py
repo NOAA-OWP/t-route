4555ab5bd1543741e2328b627abee264a14a020f
James Halgren <james.halgren@noaa.gov>
2021-02-22 10:13:43 -0600

    Blacken reservoir files

--- src/fortran_routing/Reservoir_Binding/kernels/setup.py
+++ src/fortran_routing/Reservoir_Binding/kernels/setup.py
@@ -2,38 +2,42 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-#TODO: Imitate more general usage demonstrated in src/python_routing_v02/setup.py (OR integrate directly into that file...)
+# TODO: Imitate more general usage demonstrated in src/python_routing_v02/setup.py (OR integrate directly into that file...)
 
-#Document to set this up
-#When installing this module from source
-#$LD_LIBRARY_PATH=/path/to/my/netcdf/:$LD_LIBRARY_PATH setup.py
+# Document to set this up
+# When installing this module from source
+# $LD_LIBRARY_PATH=/path/to/my/netcdf/:$LD_LIBRARY_PATH setup.py
 
 
-ext_modules=[
-    Extension("compute_kernel_lp", 
-              sources = ["compute_kernel_lp.pyx"],
-              include_dirs = [],
-              libraries=['gfortran', 'netcdff', 'netcdf'],
-              library_dirs=['/lib/gcc/x86_64-redhat-linux/4.8.5', '/usr/lib64/openmpi/lib/'],
-              extra_objects=['fortran/Reservoirs/binding_lp.a']),
-
-    Extension("compute_kernel_hybrid", 
-              sources = ["compute_kernel_hybrid.pyx"],
-              include_dirs = [],
-              libraries=['gfortran', 'netcdff', 'netcdf'],
-              library_dirs=['/lib/gcc/x86_64-redhat-linux/4.8.5', '/usr/lib64/openmpi/lib/'],
-              extra_objects=['fortran/Reservoirs/bind_hybrid.a']),
-
-    Extension("compute_kernel_rfc", 
-              sources = ["compute_kernel_rfc.pyx"],
-              include_dirs = [],
-              libraries=['gfortran', 'netcdff', 'netcdf'],
-              library_dirs=['/lib/gcc/x86_64-redhat-linux/4.8.5', '/usr/lib64/openmpi/lib/'],
-              extra_objects=['fortran/Reservoirs/bind_rfc.a'])
+ext_modules = [
+    Extension(
+        "compute_kernel_lp",
+        sources=["compute_kernel_lp.pyx"],
+        include_dirs=[],
+        libraries=["gfortran", "netcdff", "netcdf"],
+        library_dirs=["/lib/gcc/x86_64-redhat-linux/4.8.5", "/usr/lib64/openmpi/lib/"],
+        extra_objects=["fortran/Reservoirs/binding_lp.a"],
+    ),
+    Extension(
+        "compute_kernel_hybrid",
+        sources=["compute_kernel_hybrid.pyx"],
+        include_dirs=[],
+        libraries=["gfortran", "netcdff", "netcdf"],
+        library_dirs=["/lib/gcc/x86_64-redhat-linux/4.8.5", "/usr/lib64/openmpi/lib/"],
+        extra_objects=["fortran/Reservoirs/bind_hybrid.a"],
+    ),
+    Extension(
+        "compute_kernel_rfc",
+        sources=["compute_kernel_rfc.pyx"],
+        include_dirs=[],
+        libraries=["gfortran", "netcdff", "netcdf"],
+        library_dirs=["/lib/gcc/x86_64-redhat-linux/4.8.5", "/usr/lib64/openmpi/lib/"],
+        extra_objects=["fortran/Reservoirs/bind_rfc.a"],
+    ),
 ]
 
 setup(
-  name = 'RoutingKernels',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules,
+    name="RoutingKernels",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
 )
