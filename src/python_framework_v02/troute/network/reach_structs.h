1e857abe16cd337e4b7bc8cb40fd297155d0d519
Nels Frazier <nels.frazier@noaa.gov>
2021-03-01 12:07:49 -0600

    refactor top level reach, implement subtypes a C union

---
+++ src/python_framework_v02/troute/network/reach_structs.h
@@ -0,0 +1,22 @@
+#ifndef REACH_STRUCTS_H
+#define REACH_STRUCTS_H
+/*
+    C Structures
+*/
+#include "musking/mc_reach_structs.h"
+#include "reservoirs/levelpool/levelpool_structs.h"
+
+typedef union {
+  _MC_Reach mc_reach;
+  _MC_Levelpool lp;
+} _ReachUnion;
+
+typedef struct {
+  _ReachUnion reach;
+  int _num_segments; /* FIXME NOT USED HERE??? */
+  long* _upstream_ids;
+  int _num_upstream_ids;
+  int type;
+} _Reach;
+
+#endif //REACH_STRUCTS_H
