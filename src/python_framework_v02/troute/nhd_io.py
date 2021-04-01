75f72c55a8a95da03a15c72957893050e4be89da
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-04-01 11:44:54 -0400

    fix error in csv read, was missing first segment in domain mask

--- src/python_framework_v02/troute/nhd_io.py
+++ src/python_framework_v02/troute/nhd_io.py
@@ -11,7 +11,6 @@ def read_netcdf(geo_file_path):
     with xr.open_dataset(geo_file_path) as ds:
         return ds.to_dataframe()
 
-
 def read_csv(geo_file_path, header="infer", layer_string=None):
     if geo_file_path.suffix == ".zip":
         if layer_string is None:
@@ -20,7 +19,7 @@ def read_csv(geo_file_path, header="infer", layer_string=None):
             with zcsv.open(layer_string) as csv:
                 return pd.read_csv(csv, header=header)
     else:
-        return pd.read_csv(geo_file_path)
+        return pd.read_csv(geo_file_path, header=header)
 
 
 def read_geopandas(geo_file_path, layer_string=None, driver_string=None):
