fa0eb893c61f893eb07ecbc500ced3e216ea5b35
David Mattern <dmattern@cheyenne1.cheyenne.ucar.edu>
2021-04-25 16:04:20 -0600

    added new reservoir types to compute structured

--- src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
+++ src/python_routing_v02/compute_nhd_routing_SingleSeg_v02.py
@@ -685,6 +685,9 @@ def compute_nhd_routing_v02(
                 # Assumes everything else is a waterbody...
                 wbodies_segs = set(segs).symmetric_difference(common_segs)
 
+                #Declare empty dataframe
+                reservoir_types_df_sub = pd.DataFrame()
+
                 # If waterbody parameters exist
                 if not waterbodies_df.empty:
 
@@ -806,7 +809,7 @@ def compute_nhd_routing_v02(
                         lake_segs,
                         waterbodies_df_sub.values,
                         waterbody_parameters,
-                        reservoir_types_df_sub.values,
+                        reservoir_types_df_sub.values.astype("int32"),
                         reservoir_type_specified,
                         model_start_time,
                         usgs_df_sub.values.astype("float32"),
@@ -1115,6 +1118,11 @@ def main():
     # waterbodies_segments = supernetwork_values[13]
     # connections_tailwaters = supernetwork_values[4]
 
+    reservoir_type_specified = False
+
+    #Declare empty dataframe
+    reservoir_types_df_reduced = pd.DataFrame()
+
     if break_network_at_waterbodies:
         # Read waterbody parameters
         waterbodies_df = nhd_io.read_waterbody_df(
@@ -1129,7 +1137,8 @@ def main():
         )
 
         #Declare empty dataframe
-        reservoir_types_df = {}
+        reservoir_types_df = pd.DataFrame()
+        reservoir_types_df_reduced = pd.DataFrame()
 
         #Check if hybrid-usgs, hybrid-usace, or rfc type reservoirs are set to true
         wbtype="hybrid_and_rfc"
@@ -1182,8 +1191,10 @@ def main():
 
 
         else:
-            print ("========hybrid off -------------------------------")
+            reservoir_type_specified = False
 
+            #Declare empty dataframe
+            reservoir_types_df_reduced = pd.DataFrame()
 
 
     else:
