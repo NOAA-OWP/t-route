1770e46cead4d89a9037c77e3ad11a18b9e9e61d
James Halgren <james.halgren@noaa.gov>
2021-06-07 22:54:36 +0000

    add parity_check_file option to parity check in v03 operation

--- src/nwm_routing/src/nwm_routing/output.py
+++ src/nwm_routing/src/nwm_routing/output.py
@@ -19,6 +19,7 @@ def nwm_output_generator(
     debuglevel=0,
 ):
 
+    parity_check_file = parity_parameters.get("parity_check_file", None)
     parity_check_input_folder = parity_parameters.get("parity_check_input_folder", None)
     parity_check_file_index_col = parity_parameters.get(
         "parity_check_file_index_col", None
@@ -29,6 +30,9 @@ def nwm_output_generator(
     parity_check_compare_node = parity_parameters.get("parity_check_compare_node", None)
 
     # TODO: find a better way to deal with these defaults and overrides.
+    parity_set["parity_check_file"] = parity_set.get(
+        "parity_check_file", parity_check_file
+    )
     parity_set["parity_check_input_folder"] = parity_set.get(
         "parity_check_input_folder", parity_check_input_folder
     )
