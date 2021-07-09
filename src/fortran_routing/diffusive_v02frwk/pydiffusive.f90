0293da4cfb679c5bcaaa12eed826af95c8823592
Adam Wlostowski <66840445+awlostowski-noaa@users.noreply.github.com>
2021-07-09 11:54:41 -0500

    Diffusive ics (#16)
    * pass initial condition data from .pyx to diffusive_utils
    
    * correct argument positioning
    
    * plan positioning of ic array creation in workflow
    
    * typo fix
    
    * format initial conditions array for fortran diffusive kernel
    
    * pass iniq back from diffusive_utils
    
    * declare iniq array variable
    
    * pass iniq data down to diffusive fortran kernel
    
    * specify initial flow conditions in fortran kernel

--- src/fortran_routing/diffusive_v02frwk/pydiffusive.f90
+++ src/fortran_routing/diffusive_v02frwk/pydiffusive.f90
@@ -7,7 +7,7 @@ implicit none
 contains
 subroutine c_diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt_ql_g, dt_ub_g,&
         dt_db_g, nts_ql_g, nts_ub_g, nts_db_g, mxncomp_g, nrch_g, z_ar_g, bo_ar_g, traps_ar_g,&
-        tw_ar_g, twcc_ar_g, mann_ar_g, manncc_ar_g, so_ar_g, dx_ar_g, nhincr_m_g, nhincr_f_g,&
+        tw_ar_g, twcc_ar_g, mann_ar_g, manncc_ar_g, so_ar_g, dx_ar_g, iniq, nhincr_m_g, nhincr_f_g,&
         ufhlt_m_g,  ufqlt_m_g, ufhlt_f_g, ufqlt_f_g, frnw_col, frnw_g, qlat_g, ubcd_g, dbcd_g,&
         cfl_g, theta_g, tzeq_flag_g, y_opt_g, so_llm_g, ntss_ev_g, q_ev_g, elv_ev_g) bind(c)
 
@@ -18,7 +18,7 @@ subroutine c_diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt
     real(c_double), dimension(mxncomp_g, nrch_g), intent(in) :: z_ar_g, bo_ar_g, traps_ar_g, tw_ar_g, twcc_ar_g
     real(c_double), dimension(mxncomp_g, nrch_g), intent(in) :: mann_ar_g, manncc_ar_g
     real(c_double), dimension(mxncomp_g, nrch_g), intent(inout) :: so_ar_g
-    real(c_double), dimension(mxncomp_g, nrch_g), intent(in) :: dx_ar_g
+    real(c_double), dimension(mxncomp_g, nrch_g), intent(in) :: dx_ar_g, iniq
     integer(c_int), intent(in) :: nhincr_m_g, nhincr_f_g
     real(c_double), dimension(mxncomp_g, nrch_g, nhincr_m_g), intent(in) :: ufhlt_m_g,  ufqlt_m_g
     real(c_double), dimension(mxncomp_g, nrch_g, nhincr_f_g), intent(in) :: ufhlt_f_g, ufqlt_f_g
@@ -36,7 +36,7 @@ subroutine c_diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt
     
     call diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt_ql_g, dt_ub_g,&
         dt_db_g, nts_ql_g, nts_ub_g, nts_db_g, mxncomp_g, nrch_g, z_ar_g, bo_ar_g, traps_ar_g,&
-        tw_ar_g, twcc_ar_g, mann_ar_g, manncc_ar_g, so_ar_g, dx_ar_g, nhincr_m_g, nhincr_f_g,&
+        tw_ar_g, twcc_ar_g, mann_ar_g, manncc_ar_g, so_ar_g, dx_ar_g, iniq, nhincr_m_g, nhincr_f_g,&
         ufhlt_m_g,  ufqlt_m_g, ufhlt_f_g, ufqlt_f_g, frnw_col, frnw_g, qlat_g, ubcd_g, dbcd_g,&
         cfl_g, theta_g, tzeq_flag_g, y_opt_g, so_llm_g, ntss_ev_g, q_ev_g, elv_ev_g)
     
