65fc089c47ebe0d2c7de2a5522011927a94712f7
James Halgren <james.halgren@noaa.gov>
2021-08-10 20:39:08 -0500

    comment update

--- src/python_routing_v02/troute/routing/fast_reach/simple_da.pyx
+++ src/python_routing_v02/troute/routing/fast_reach/simple_da.pyx
@@ -10,12 +10,13 @@ cdef float simple_da_with_decay(
     """
     Given a modeled value, last valid observation,
     time since that observation, and an exponential
-    decay_coefficient, compute the nudge value
+    decay_coefficient, compute the 'nudged' value
     """
 
     cdef float da_weight, da_shift, da_weighted_shift
     da_weight = exp(minutes_since_last_valid/-decay_coeff)  # TODO: This could be pre-calculated knowing when obs finish relative to simulation time
     # TODO: we need to be able to export these values to compute the 'Nudge'
+    # One possibility would be to return only the nudge from this function...
     da_shift = last_valid_obs - model_val
     da_weighted_shift = da_shift * da_weight
     # printf("%g %g %g %g %g --> %g\n", minutes_since_last_valid, decay_coeff, da_shift, da_weighted_shift, model_val, model_val + da_weighted_shift)
