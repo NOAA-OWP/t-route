59ea82c835799008af8cd8c61e0f0346af4fea31
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-08-18 10:15:23 -0400

    diffusive_cnt f90 files

---
+++ src/fortran_routing/diffusive_v02frwk/diffusive_cnt/constants_module.f90
@@ -0,0 +1,9 @@
+module constants_module
+
+    implicit none
+    save
+
+    double precision, parameter :: grav = 9.81
+    double precision, parameter :: TOLERANCE = 1e-8
+
+end module constants_module
