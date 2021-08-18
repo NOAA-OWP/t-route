6566f5a5b7ab51eb4d5b50d5277d6b3ecfafe7a1
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-08-18 09:44:31 -0400

    include cnt model in compile.sh

--- src/python_routing_v02/compiler.sh
+++ src/python_routing_v02/compiler.sh
@@ -6,7 +6,8 @@ REPOROOT=`pwd`
 #For each build step, you can set these to true to make it build
 #or set it to anything else (or unset) to skip that step
 build_mc_kernel=true
-build_diffusive_kernel=true
+build_diffusive_cnx_kernel=true
+build_diffusive_cnt_kernel=true
 build_reservoir_kernel=true
 build_framework=true
 build_routing=true
@@ -32,12 +33,28 @@ if  [[ "$build_mc_kernel" == true ]]; then
   make install || exit
 fi
 
-if  [[ "$build_diffusive_kernel" == true ]]; then
+if  [[ "$build_diffusive_cnx_kernel" == true ]]; then
   #building reach and resevoir kernel files .o  
   cd $REPOROOT/src/fortran_routing/diffusive_v02frwk/
   make clean
   make diffusive.o
   make pydiffusive.o
+  make install || exit
+fi
+
+if  [[ "$build_diffusive_cnt_kernel" == true ]]; then
+  #building reach and resevoir kernel files .o  
+  cd $REPOROOT/src/fortran_routing/diffusive_v02frwk/diffusive_cnt/
+  make clean
+  make arrays_module.o
+  make matrix_module.o
+  make var_module.o
+  make arrays_section_module.o
+  make xsec_attribute_module.o
+  make constants_module.o
+  make subtools.o
+  make diffusive_cnt.o
+  make pydiffusive_cnt.o
   make install || exit
 fi
 
