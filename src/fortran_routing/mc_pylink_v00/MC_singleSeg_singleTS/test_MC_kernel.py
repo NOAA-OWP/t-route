7dd5a6795741a9f66a773dd415407d55d8b5a667
James Halgren <james.halgren@noaa.gov>
2021-08-26 12:49:02 -0500

    update/streamline test files

--- src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/test_MC_kernel.py
+++ src/fortran_routing/mc_pylink_v00/MC_singleSeg_singleTS/test_MC_kernel.py
@@ -3,51 +3,6 @@ import mc_sseg_stime_NOLOOP_demo as demo
 from test_suite_parameters import generate_conus_MC_parameters
 
 
-"""
-Statistics from the CONUS NWM 2.1 dataset
-            mean     std_dev      min        25%         50%          75%           max
-dx    1,947.776    1,965.625     1.000   558.000   1,549.000    2,631.000    95,714.000 
-bw        5.290        9.407     0.135     1.904       2.769        4.889       230.035 
-tw        8.817       15.678     0.225     3.173       4.615        8.148       383.392 
-twcc     26.450       47.033     0.674     9.518       13.845      24.443     1,150.175 
-n         0.058        0.003     0.040     0.060       0.060        0.060         0.060 
-ncc       0.117        0.006     0.080     0.120       0.120        0.120         0.120 
-cs        0.5857       0.1945    0.0846    0.4625      0.5944       0.7012        2.254 
-s0        0.02150      0.04585   0.00001   0.00100     0.00600      0.01900       4.600 
-qlat                              1       500000
-qup                               1       500000
-quc                               1       500000
-qdp                               1       500000
-depthp                            1       100
-dt==300
-
-*Legend*
-dt = Time step
-dx = segment length
-bw = Trapezoidal bottom width
-tw = Channel top width (at bankfull)
-twcc = Flood plain width
-n_manning = manning roughness of channel
-n_manning_cc = manning roughness of floodplain
-cs = channel trapezoidal sideslope
-s0 = downstream segment bed slope
-qlat = Lateral inflow in this time step
-qup = inflow into segment from upstream in previous timestep
-quc = inflow into segment from upstream in current timestep
-qdp = outflow from segment in previous timestep
-depthp = depth in segment in previous timestep
-qdc = outflow from segment in current timestep (MC solution yields this value)
-
-*Notes:*
-ncc == 2 * n
-
-twcc == 3 * tw
-
-tw >= bw
-
-prior tests have varied dt, even though it is always held at 300 for NWM simulations to date.
-"""
-
 input_tup = (
     "single",  # calculation precision (NOT USED)
     300.0,  # Time step
@@ -68,7 +23,7 @@ input_tup = (
 
 # qdc1, qdc2, velc1, velc2, depthc1, depthc2 = demo.compare_methods(*input_tup)
 
-input_tup_random = generate_conus_MC_parameters(3, 10)
+input_tup_random = generate_conus_MC_parameters(1, 16)
 out = demo.compare_methods("single", *(next(input_tup_random)))
 # print(out)
 qdc1, qdc2, velc1, velc2, depthc1, depthc2 = out
