3364de84d4a58aca1fe44e26655508eccdd39bcf
David Mattern <dmattern@cheyenne2.cheyenne.ucar.edu>
2021-08-30 15:30:18 -0600

    changes to run mc_reach normal

--- src/python_routing_v02/troute/routing/compute.py
+++ src/python_routing_v02/troute/routing/compute.py
@@ -1070,10 +1070,10 @@ def compute_nhd_routing_v02(
             - timedelta(seconds=qlat_time_step_seconds)
 
             #Original
-            #model_start_time = model_start_time_datetime_object.strftime('%Y-%m-%d_%H:%M:%S')
+            model_start_time = model_start_time_datetime_object.strftime('%Y-%m-%d_%H:%M:%S')
 
             #Quick fix for now for nexus time because has an extra space at the beginning of the string
-            model_start_time = model_start_time_datetime_object.strftime(' %Y-%m-%d_%H:%M:%S')
+            #model_start_time = model_start_time_datetime_object.strftime(' %Y-%m-%d_%H:%M:%S')
 
             param_df_sub = param_df_sub.reindex(
                 param_df_sub.index.tolist() + lake_segs
