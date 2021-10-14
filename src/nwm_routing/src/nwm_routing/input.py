70447385d46eabf442fdac44cb04ae5f187e57c3
Jacob Hreha <jhreha@cheyenne2.cheyenne.ucar.edu>
2021-10-14 14:43:57 -0600

    saving mandatory check progress

--- src/nwm_routing/src/nwm_routing/input.py
+++ src/nwm_routing/src/nwm_routing/input.py
@@ -227,21 +227,29 @@ def check_inputs(
     try:
         log_parameters['debuglevel']
     except KeyError as err:
-        errmsg = 'No debuglevel selected. The default value is 0. Please enter a debuglevel inside log_parameters. Ex. debuglevel: 1'
+        errmsg = 'No debuglevel selected. The default value is 0. Please enter a debuglevel inside log_parameters. Example: debuglevel: 1'
         print(errmsg)
         raise err
 
     try:
-        supernetwork_parameters['geo_file_path']
+        J = pathlib.Path(supernetwork_parameters['geo_file_path'])
+        assert J.is_file() == True
+    except AssertionError:
+        print("Aborting simulation because forcing file", J, "cannot be not found.")
+        raise
     except KeyError as err:
-        errmsg = 'No geo_file_path selected. Select a string, file path to directory containing channel geometry data. Ex. "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc"'
+        errmsg = 'No geo_file_path selected. Select a string, file path to directory containing channel geometry data. Example: "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc"'
         print(errmsg)
         raise err
 
     try:
-        supernetwork_parameters['geo_file_path']
+        J = pathlib.Path(supernetwork_parameters['geo_file_path'])
+        assert J.is_file() == True
+    except AssertionError:
+        print("Aborting simulation because forcing file", J, "cannot be not found.")
+        raise
     except KeyError as err:
-        errmsg = 'No geo_file_path selected. Select a string, file path to directory containing channel geometry data. Ex. "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc"'
+        errmsg = 'No geo_file_path selected. Select a string, file path to directory containing channel geometry data. Example: "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc"'
         print(errmsg)
         raise err
 
@@ -277,13 +285,112 @@ def check_inputs(
             'musx: "MusX"',
             'cs: "ChSlp"']
     try:
-        for loc, i in enumerate(columns):
-            supernetwork_parameters['columns'][i]
+        for loc, col in enumerate(columns):
+            supernetwork_parameters['columns'][col]
     except KeyError as err:
-        errmsg = '{} not selected. Select a string, {}. Ex. {}'.format(i,col_definitions[loc],col_examples[loc])
+        errmsg = '{} not selected. Select a string, {}. Example: {}'.format(col,col_definitions[loc],col_examples[loc])
         print(errmsg)
         raise err
-    import pdb; pdb.set_trace()
+
+
+    if supernetwork_parameters['geo_file_path'][-13:] == "Route_Link.nc":
+        try:
+            supernetwork_parameters['waterbody_null_code']
+        except KeyError as err:
+            errmsg = 'No waterbody_null_code selected. Select a null value. The coding in channel gemetry dataset for non waterbody segments under attribute named in `columns: waterbody`. Example: -9999'
+            print(errmsg)
+            raise err
+
+    #How should we check if a run contains waterbodies? Could write a check then to make sure break_network_at_waterbodies is set to True.
+    if waterbody_parameters['break_network_at_waterbodies']:
+        columns = ['level_pool_waterbody_parameter_file_path', 'level_pool_waterbody_id', 'level_pool_waterbody_area', 'level_pool_weir_elevation',
+        'level_pool_waterbody_max_elevation', 'level_pool_outfall_weir_coefficient', 'level_pool_outfall_weir_length', 'level_pool_overall_dam_length',
+        'level_pool_orifice_elevation', 'level_pool_orifice_coefficient', 'level_pool_orifice_area']   
+        col_definitions = ['filepath to waterbody parameter file (LAKEPARM.nc)',
+        'level_pool_waterbody_id',
+        'level_pool_waterbody_area',
+        'level_pool_weir_elevation',
+        'level_pool_waterbody_max_elevation',
+        'level_pool_outfall_weir_coefficient',
+        'level_pool_outfall_weir_length',
+        'level_pool_overall_dam_length',
+        'level_pool_orifice_elevation',
+        'level_pool_orifice_coefficient',
+        'level_pool_orifice_area']
+        col_examples = ['level_pool_waterbody_parameter_file_path: tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/LAKEPARM.nc',
+                'level_pool_waterbody_id: lake_id',
+                'level_pool_waterbody_area: LkArea',
+                'level_pool_weir_elevation: WeirE',
+                'level_pool_waterbody_max_elevation: LkMxE',
+                'level_pool_outfall_weir_coefficient: WeirC',
+                'level_pool_outfall_weir_length: WeirL',
+                'level_pool_overall_dam_length: DamL',
+                'level_pool_orifice_elevation: OrificeE',
+                'level_pool_orifice_coefficient: OrificeC',
+                'level_pool_orifice_area: OrificeA']
+        try:
+            for loc, col in enumerate(columns):
+                waterbody_parameters['level_pool'][col]
+        except KeyError as err:
+            errmsg = '{} not selected. Select a string, the {} column name. Example: {}'.format(col,col_definitions[loc],col_examples[loc])
+            print(errmsg)
+            raise err
+
+    try:
+        compute_parameters['compute_kernel']
+    except KeyError as err:
+        errmsg = 'No compute_kernel selected. Will default to "V02-structured" - Muskingum Cunge. \
+        Please enter a compute_kernel inside compute_parameters. Example: compute_kernel: V02-structured'
+        print(errmsg)
+        options = ['V02-structured - Muskingum Cunge','diffusive - Diffusive with adaptive timestepping' ,'diffusice_cnt - Diffusive with CNT numerical solution']
+        for option in options:
+            print("Options: {}".format(option))
+        raise err
+
+    if compute_parameters['restart_parameters']['wrf_hydro_channel_restart_file']:
+        columns = ['wrf_hydro_channel_ID_crosswalk_file', 'wrf_hydro_channel_ID_crosswalk_file_field_name', 'wrf_hydro_channel_restart_upstream_flow_field_name',
+        'wrf_hydro_channel_restart_downstream_flow_field_name',
+        'wrf_hydro_channel_restart_depth_flow_field_name', 'wrf_hydro_waterbody_ID_crosswalk_file', 'wrf_hydro_waterbody_ID_crosswalk_file_field_name',
+        'wrf_hydro_waterbody_crosswalk_filter_file', 'wrf_hydro_waterbody_crosswalk_filter_file_field_name']   
+        col_definitions = [
+        'channel geometry file',
+        'segment IDs in restart file',
+        'upstream flow in restart file',
+        'downstream flow in restart file',
+        'depth in restart file',
+        'lake parameter file',
+        'waterbody ID',
+        'channel geometry file',
+        'waterbody IDs in channel geometry file']
+        col_examples = ['wrf_hydro_channel_ID_crosswalk_file: tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc',
+                'wrf_hydro_channel_ID_crosswalk_file_field_name: link',
+                'wrf_hydro_channel_restart_upstream_flow_field_name: qlink1',
+                'wrf_hydro_channel_restart_downstream_flow_field_name: qlink2',
+                'wrf_hydro_channel_restart_depth_flow_field_name: hlink',
+                'wrf_hydro_waterbody_ID_crosswalk_file: "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/LAKEPARM.nc"',
+                'wrf_hydro_waterbody_ID_crosswalk_file_field_name: lake_id',
+                'wrf_hydro_waterbody_crosswalk_filter_file: "tmp_florence_run_nudging/run_nudging_nwm_channel-only/NWM/DOMAIN/Route_Link.nc"',
+                'wrf_hydro_waterbody_crosswalk_filter_file_field_name: NHDWaterbodyComID']
+        try:
+            for loc, col in enumerate(columns):
+                waterbody_parameters['level_pool'][col]
+        except KeyError as err:
+            errmsg = '{} not selected. Select a string, the {} column name or filepath. Example: {}'.format(col,col_definitions[loc],col_examples[loc])
+            print(errmsg)
+            raise err
+        import pdb; pdb.set_trace()
+    # try:
+    #     supernetwork_parameters['synthetic_wb_segments']
+    # except KeyError as err:
+    #     errmsg = 'No synthetic_wb_segments selected. Select a synthetic values if using NWM 2.1 or 3.0 Route_Link.nc. Synthetic waterbody segment IDs that are used to construct the Great Lakes. \
+    #     These segments appear in the NWM 2.1 and 3.0 Route_Link.nc files but are not needed in the routing computation. Example: synthetic_wb_segments:'
+    #     print(errmsg)
+    #     example_list = [4800002,4800004,4800006,4800007]
+    #     for ex in example_list:
+    #         print("- {}".format(ex))
+    #     raise err
+
+
         # check that all forcing files exist
 # for f in forcing_filename_list:
 #     try:
