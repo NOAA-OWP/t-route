563897674cad258b3239908a8406f79bdf871bc9
Jacob Hreha <jhreha@cheyenne2.cheyenne.ucar.edu>
2021-10-22 08:19:13 -0600

    moved logging setting to input.py to trigger yaml check based on log level

--- src/nwm_routing/src/nwm_routing/log_level_set.py
+++ src/nwm_routing/src/nwm_routing/log_level_set.py
@@ -3,7 +3,7 @@ import logging
 import sys   
 
 def log_level_set(input_parameters):
-    log_level = input_parameters.get("log_level", None)
+    log_level = input_parameters.get("log_level", 'DEBUG')
     logging.basicConfig(
         level=log_level,
         format='%(levelname)7s: %(message)s',
