ca01178de517a72bc9a56f2346e15e3e7f9af0de
awlostowski-noaa <adamw@cheyenne3.cheyenne.ucar.edu>
2021-10-26 11:15:28 -0600

    add date to logging messages

--- src/nwm_routing/src/nwm_routing/log_level_set.py
+++ src/nwm_routing/src/nwm_routing/log_level_set.py
@@ -5,6 +5,6 @@ def log_level_set(input_parameters):
     log_level = input_parameters.get("log_level", 'DEBUG')
     logging.basicConfig(
         level=log_level,
-        format='%(levelname)7s [%(filename)s:%(lineno)s - %(funcName)20s()]: %(message)s',
+        format='%(asctime)s %(levelname)7s [%(filename)s:%(lineno)s - %(funcName)20s()]: %(message)s',
         stream=sys.stderr,
     )
