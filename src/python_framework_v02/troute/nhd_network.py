a0ee1622693c81aa36d332f391aedc4527e2f9db
Nels Frazier <nels.frazier@noaa.gov>
2021-11-30 19:18:09 +0000

    deprecate extract_waterbody_connections in nhd_network

--- src/python_framework_v02/troute/nhd_network.py
+++ src/python_framework_v02/troute/nhd_network.py
@@ -3,7 +3,9 @@ from itertools import chain
 from functools import reduce, partial
 from collections.abc import Iterable
 from toolz import pluck
-
+from deprecated import deprecated
+#Consider using sphinx for inlining deprecation into docstrings
+#from deprecated.sphinx import deprecated
 
 def reverse_dict(d):
     """
@@ -76,7 +78,7 @@ def extract_connections(rows, target_col, terminal_codes=None):
             network[src].append(dst)
     return network
 
-
+@deprecated(version='2.0', reason="Functionality moved to Network classes")
 def extract_waterbody_connections(rows, target_col, waterbody_null=-9999):
     """Extract waterbody mapping from dataframe."""
     return (
