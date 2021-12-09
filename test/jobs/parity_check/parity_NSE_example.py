ea3401f08ac66f06eaf565253799c62145a3a585
Jacob Hreha <jhreha@cheyenne2.cheyenne.ucar.edu>
2021-12-09 14:02:28 -0700

    removed yaml examples, changed parity hse outputs, condensed arg passing in bash files to python

--- test/jobs/parity_check/parity_NSE_example.py
+++ test/jobs/parity_check/parity_NSE_example.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import hydroeval as he
 import math 
+import sys
 # import matplotlib.pyplot as plt
 
-s1 = pd.read_pickle("/glade/u/home/jhreha/t-route/test/jobs/parity_check/chanobs_output/example1")
-s2 = pd.read_pickle("/glade/u/home/jhreha/t-route/test/jobs/parity_check/chanobs_output/example2")
+s1 = pd.read_pickle(sys.argv[1])
+s2 = pd.read_pickle(sys.argv[2])
 
 s1_q = s1.iloc[:,::3].sort_index()
 s2_q = s2.iloc[:,::3].sort_index()
@@ -35,28 +36,27 @@ else:
        
     print("****************************************")
     print("**** Are simulated flows similar? ******")
-    print(" ** (... within 0.1% of one another) ***")
+    print(" ** (... > .99 of the Nash–Sutcliffe model efficiency coefficient (max 1)...) **")
     print("****************************************")
     print("\n")
     diff_frac = abs(s1_q - s2_q)/s1_q
     col_idx = diff_frac.max().argmax()
     row_idx = diff_frac.iloc[:,col_idx].argmax()
 
-    if diff_frac.max().max() < 0.001:
+    nse_list = []
+    for id in s2_q.index: nse_list.append(he.evaluator(he.nse,s2_q.loc[id].dropna().to_numpy().flatten(), s1_q.loc[id].dropna().to_numpy().flatten())[0])
+    nse_list_filtered = [x for x in nse_list if math.isnan(x) == False]
+
+    if min(nse_list_filtered) > .99:
         print("YES! - Simulation results are very close")
+        print("Minimum nse:", min(nse_list_filtered))
     else:
         print("NO. Simulation results are unacceptably different.")
-        print("The largest proportional flow difference is: ", diff_frac.max().max() * 100, "%") 
+        print("The smallest Nash–Sutcliffe model efficiency coefficient is: ", min(nse_list_filtered)) 
         print("at link ID", s1_q.index[col_idx], "and timestep", row_idx)
         print("simulation 1 reports a flow value of", s1_q.iloc[row_idx, col_idx])
         print("simulation 2 reports a flow value of", s2_q.iloc[row_idx, col_idx])
 
-    overall_nse = he.evaluator(he.nse,s2_q.to_numpy().flatten(), s1_q.to_numpy().flatten())
-    nse_list = []
-    for id in s2_q.index: nse_list.append(he.evaluator(he.nse,s2_q.loc[id].dropna().to_numpy().flatten(), s1_q.loc[id].dropna().to_numpy().flatten())[0])
-    nse_list_filtered = [x for x in nse_list if math.isnan(x) == False]
-    print("Overall nse:", overall_nse)
-    print("Minimum nse:", min(nse_list_filtered))
     # plt.hist(nse_list_filtered, density=True, bins=30)
     # plt.ylabel('NSE')
     # plt.xlabel('Data')
--- test/jobs/parity_check/test_example.sh
+++ test/jobs/parity_check/test_example.sh
@@ -1,11 +1,25 @@
 #Remove outputs from previous runs.
 rm /glade/u/home/jhreha/t-route/test/jobs/parity_check/chanobs_output/example1
 rm /glade/u/home/jhreha/t-route/test/jobs/parity_check/chanobs_output/example2
+
+#Each yaml file needs to have test outputs turned on and have a destination folder.
+# Example
+#output_parameters:
+#    test_output: ../../t-route/test/jobs/parity_check/chanobs_output/example1
+#    chanobs_output: 
+#         chanobs_output_directory: ../../t-route/test/jobs/parity_check/chanobs_output/chanobs_output_test.ncdf
+
 #Creates master branch outputs to use for benchmark.
-git checkout master #master
+# git checkout master #master
 python -m nwm_routing -f -V3 parity_florence_example1.yaml 
+
 #Creates test branch outputs for comparison against benchmark. 
-git checkout adam/make_chanobs #test branch
+# git checkout adam/make_chanobs #test branch
 python -m nwm_routing -f -V3 parity_florence_example2.yaml 
-#Runs the hydrotools Nash–Sutcliffe model efficiency coefficient tests to check for acceptable parity levels. 
-python parity_NSE_generalized.py
+
+#Saves a bash variable of the output directories for each file.
+yaml1="$(cat parity_florence_example1.yaml | shyaml get-value output_parameters.test_output)"
+yaml2="$(cat parity_florence_example2.yaml | shyaml get-value output_parameters.test_output)"
+
+# #Runs the hydrotools Nash–Sutcliffe model efficiency coefficient tests to check for acceptable parity levels. 
+python parity_NSE_example.py $yaml2 $yaml1
