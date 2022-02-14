e5cf14c0b6f0b86be7b6c4b60c7284761b839648
awlostowski-noaa <adamw@cheyenne4.cheyenne.ucar.edu>
2022-02-14 14:26:39 -0700

    docstring log_level_set

--- src/troute-nwm/src/nwm_routing/log_level_set.py
+++ src/troute-nwm/src/nwm_routing/log_level_set.py
@@ -2,6 +2,23 @@ import logging
 import sys   
 
 def log_level_set(input_parameters):
+    '''
+    Set logging level and specify logger configuration.
+    
+    Arguments
+    ---------
+    input_parameters (dict): User input logging parameters
+    
+    Returns
+    -------
+    None
+    
+    Notes
+    -----
+    In the absense of user-specified logging level, level defaults to DEGUG
+    See also https://docs.python.org/3/library/logging.html
+    
+    '''
     log_level = input_parameters.get("log_level", 'DEBUG')
     logging.basicConfig(
         level=log_level,
