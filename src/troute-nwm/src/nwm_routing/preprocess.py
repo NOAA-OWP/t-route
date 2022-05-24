d03ed6b48543e77529c366a19f23791e0c0c3be7
Dong Kim <donghakim@cheyenne6.cheyenne.ucar.edu>
2022-05-24 08:52:51 -0600

    cleaned preprocess with new diffusive_domain yaml

--- src/troute-nwm/src/nwm_routing/preprocess.py
+++ src/troute-nwm/src/nwm_routing/preprocess.py
@@ -338,15 +338,9 @@ def nwm_network_preprocess(
             diffusive_network_data[tw] = {}
 
             # add diffusive domain segments
-            #diffusive_network_data[tw]['mainstem_segs'] = diffusive_domain[tw]
             diffusive_network_data[tw]['mainstem_segs'] =  mainstem_segs
             # diffusive domain tributary segments
             trib_segs = []
-            #for s in diffusive_domain[tw]:
-            #    us_list = rconn[s]
-            #    for u in us_list:
-            #        if u not in diffusive_domain[tw]:
-            #            trib_segs.append(u)
             for seg in mainstem_segs:
                 us_list = rconn[seg]
                 for u in us_list:
@@ -356,7 +350,6 @@ def nwm_network_preprocess(
             diffusive_network_data[tw]['tributary_segments'] = trib_segs
 
             # diffusive domain connections object
-            #diffusive_network_data[tw]['connections'] = {k: connections[k] for k in (diffusive_domain[tw] + trib_segs)}
             diffusive_network_data[tw]['connections'] = {k: connections[k] for k in (mainstem_segs + trib_segs)}
             
             # diffusive domain reaches and upstream connections. 
@@ -371,7 +364,6 @@ def nwm_network_preprocess(
             
             # RouteLink parameters
             diffusive_network_data[tw]['param_df'] = param_df.filter(
-                #(diffusive_domain[tw] + trib_segs),
                 (mainstem_segs + trib_segs),
                 axis = 0,
             )
@@ -401,11 +393,9 @@ def nwm_network_preprocess(
         
             # ==== remove diffusive domain segs from MC domain ====        
             # drop indices from param_df
-            #param_df = param_df.drop(diffusive_domain[tw])
             param_df = param_df.drop(mainstem_segs)
         
             # remove keys from connections dictionary
-            #for s in diffusive_domain[tw]:
             for s in mainstem_segs:
                 connections.pop(s)
             
