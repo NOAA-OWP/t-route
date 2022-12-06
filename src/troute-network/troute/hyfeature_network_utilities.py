40055eb8eaa14185cca7acc60a039fff87425c32
Sean Horvath <sean.horvath@noaa.gov>
2022-12-06 16:16:04 +0000

    only raise error if .parquet files already exist. Other files are ok

--- src/troute-network/troute/hyfeature_network_utilities.py
+++ src/troute-network/troute/hyfeature_network_utilities.py
@@ -49,8 +49,8 @@ def build_forcing_sets(
             raise(RuntimeError("No output binary qlat folder supplied in config"))
         elif not os.path.exists(binary_folder):
             raise(RuntimeError("Output binary qlat folder supplied in config does not exist"))
-        elif len(os.listdir(binary_folder)) != 0:
-            raise(RuntimeError("Output binary qlat folder supplied in config is not empty"))
+        elif len(list(pathlib.Path(binary_folder).glob('*.parquet'))) != 0:
+            raise(RuntimeError("Output binary qlat folder supplied in config is not empty (already contains '.parquet' files)"))
 
         #Add tnx for backwards compatability
         nexus_files_list = list(nexus_files) + list(nexus_input_folder.glob('tnx*.csv'))
