ef8875d59f9102a337f6be780223b5981eadebe4
Sean Horvath <sean.horvath@noaa.gov>
2023-03-02 23:52:18 +0000

    edit bmi module to lazily initialize t-route network features on first call of update()

--- src/bmi_troute.py
+++ src/bmi_troute.py
@@ -14,7 +14,7 @@ class bmi_troute(Bmi):
     def __init__(self):
         """Create a Bmi troute model that is ready for initialization."""
         super(bmi_troute, self).__init__()
-        #self._model = None
+        self._model = None
         self._values = {}
         #self._var_units = {}
         self._var_loc = "node"
@@ -43,14 +43,45 @@ class bmi_troute(Bmi):
     #---------------------------------------------
     # Input variable names (CSDMS standard names)
     #---------------------------------------------
-    _input_var_names = ['land_surface_water_source__volume_flow_rate',
-                        'coastal_boundary__depth', #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                        'usgs_gage_observation__volume_flow_rate', #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                        'reservoir_usgs_gage_observation__volume_flow_rate', #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                        'reservoir_usace_gage_observation__volume_flow_rate', #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                        'rfc_gage_observation__volume_flow_rate' #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                        'lastobs__volume_flow_rate' #FIXME: this variable isn't a standard CSDMS name...couldn't find one more appropriate
-                       ]
+    _input_var_names = [
+        # segment static variables
+        'segment_id', 
+        'segment_toid',
+        'dx',
+        'n',
+        'ncc',
+        's0',
+        'bw',
+        'tw',
+        'twcc',
+        'alt',
+        'musk',
+        'musx',
+        'cs',
+        # waterbody static variables
+        'waterbody_id',
+        'waterbody_toid',
+        'LkArea',
+        'LkMxE',
+        'OrificeA',
+        'OrificeC',
+        'OrificeE',
+        'WeirC',
+        'WeirE',
+        'WeirL',
+        'ifd',
+        'qd0',
+        'h0',
+        'reservoir_type',
+        # dynamic forcing/DA variables
+        'land_surface_water_source__volume_flow_rate',
+        'coastal_boundary__depth', 
+        'usgs_gage_observation__volume_flow_rate',
+        'reservoir_usgs_gage_observation__volume_flow_rate',
+        'reservoir_usace_gage_observation__volume_flow_rate', 
+        'rfc_gage_observation__volume_flow_rate' 
+        'lastobs__volume_flow_rate' 
+        ]
 
     #---------------------------------------------
     # Output variable names (CSDMS standard names)
@@ -98,11 +129,20 @@ class bmi_troute(Bmi):
 
     #-------------------------------------------------------------------
     def initialize(self, bmi_cfg_file=None):
-
-        args = tr._handle_args_v03(['-f', bmi_cfg_file])
         
         # -------------- Read in the BMI configuration -------------------------#
         bmi_cfg_file = Path(bmi_cfg_file)
+        
+        # ------------- Initialize t-route model ------------------------------#
+        self._model(bmi_cfg_file)
+
+        
+
+        
+
+        # Set number of time steps (1 hour)
+        self._nts = 12
+
         # ----- Create some lookup tabels from the long variable names --------#
         self._var_name_map_long_first = {long_name:self._var_name_units_map[long_name][0] for \
                                          long_name in self._var_name_units_map.keys()}
@@ -111,57 +151,9 @@ class bmi_troute(Bmi):
         self._var_units_map = {long_name:self._var_name_units_map[long_name][1] for \
                                           long_name in self._var_name_units_map.keys()}
         
-        # This will direct all the next moves.
-        if bmi_cfg_file is not None:
 
-            with bmi_cfg_file.open('r') as fp:
-                cfg = yaml.safe_load(fp)
-            self._cfg_bmi = self._parse_config(cfg)
-        else:
-            print("Error: No configuration provided, nothing to do...")
         
-        # ------------- Initialize t-route model ------------------------------#
-        (
-            self._log_parameters, 
-            self._preprocessing_parameters, 
-            self._supernetwork_parameters, 
-            self._waterbody_parameters, 
-            self._compute_parameters, 
-            self._forcing_parameters, 
-            self._restart_parameters, 
-            self._hybrid_parameters, 
-            self._output_parameters, 
-            self._parity_parameters, 
-            self._data_assimilation_parameters,
-        ) = tr._input_handler_v03(args)
-
-        self._run_parameters = {
-            'dt': self._forcing_parameters.get('dt'),
-            'nts': self._forcing_parameters.get('nts'),
-            'cpu_pool': self._compute_parameters.get('cpu_pool')
-            }
-
-        self._network = tr.HYFeaturesNetwork(
-            self._supernetwork_parameters,
-            waterbody_parameters=self._waterbody_parameters,
-            restart_parameters=self._restart_parameters,
-            forcing_parameters=self._forcing_parameters,
-            data_assimilation_parameters=self._data_assimilation_parameters,
-            compute_parameters=self._compute_parameters,
-            hybrid_parameters=self._hybrid_parameters,
-            verbose=False, showtiming=False) 
         
-        # create empty data assimilation object that will get values during 'update' function. 
-        #TODO: Edit this to be done in DataAssimilation module, EmptyDA?
-        self._data_assimilation = tr.AllDA(
-            self._data_assimilation_parameters,
-            self._run_parameters,
-            self._waterbody_parameters,
-            self._network,
-            [])
-
-        # Set number of time steps (1 hour)
-        self._nts = 12
         
         # -------------- Initalize all the variables --------------------------# 
         # -------------- so that they'll be picked up with the get functions --#
@@ -191,93 +183,13 @@ class bmi_troute(Bmi):
         self._time_step = self._forcing_parameters.get('dt')
         self._time_units = 's'
     
-
     def update(self):
         """Advance model by one time step."""
-                
-        # Set input data into t-route objects
-        self._network._qlateral = pd.DataFrame(self._values['land_surface_water_source__volume_flow_rate'],
-                                                            index=self._network.segment_index.to_numpy())
-        self._network._coastal_boundary_depth_df = pd.DataFrame(self._values['coastal_boundary__depth'])
-
-
-        # Create data assimilation object from da_sets for first loop iteration
-        #TODO I'm not sure if this is the best way to do this. How will all of these variables be 
-        # fed to t-route BMI?
-        self._data_assimilation._usgs_df = pd.DataFrame(self._values['usgs_gage_observation__volume_flow_rate'])
-        self._data_assimilation._last_obs_df = pd.DataFrame(self._values['lastobs__volume_flow_rate'])
-        self._data_assimilation._reservoir_usgs_df = pd.DataFrame(self._values['reservoir_usgs_gage_observation__volume_flow_rate'])
-        self._data_assimilation._reservoir_usace_df = pd.DataFrame(self._values['reservoir_usace_gage_observation__volume_flow_rate'])
-
-
-        # Run routing
-        (
-            self._run_results, 
-            self._subnetwork_list
-        ) = tr.nwm_route(self._network.connections, 
-                         self._network.reverse_network, 
-                         self._network.waterbody_connections, 
-                         self._network._reaches_by_tw,
-                         self._compute_parameters.get('parallel_compute_method','serial'), 
-                         self._compute_parameters.get('compute_kernel'),
-                         self._compute_parameters.get('subnetwork_target_size'),
-                         self._compute_parameters.get('cpu_pool'),
-                         self._network.t0,
-                         self._time_step,
-                         self._nts,
-                         self._forcing_parameters.get('qts_subdivisions', 12),
-                         self._network.independent_networks, 
-                         self._network.dataframe,
-                         self._network.q0,
-                         self._network._qlateral,
-                         self._data_assimilation.usgs_df,
-                         self._data_assimilation.lastobs_df,
-                         self._data_assimilation.reservoir_usgs_df,
-                         self._data_assimilation.reservoir_usgs_param_df,
-                         self._data_assimilation.reservoir_usace_df,
-                         self._data_assimilation.reservoir_usace_param_df,
-                         self._data_assimilation.assimilation_parameters,
-                         self._compute_parameters.get('assume_short_ts', False),
-                         self._compute_parameters.get('return_courant', False),
-                         self._network._waterbody_df,
-                         self._waterbody_parameters,
-                         self._network._waterbody_types_df,
-                         self._network.waterbody_type_specified,
-                         self._network.diffusive_network_data,
-                         self._network.topobathy_df,
-                         self._network.refactored_diffusive_domain,
-                         self._network.refactored_reaches,
-                         [], #subnetwork_list,
-                         self._network.coastal_boundary_depth_df,
-                         self._network.unrefactored_topobathy_df,)
         
-        # update initial conditions with results output
-        self._network.new_q0(self._run_results)
-        self._network.update_waterbody_water_elevation()               
-        
-        # update t0
-        self._network.new_t0(self._time_step,self._nts)
-
-        # get reservoir DA initial parameters for next loop iteration
-        self._data_assimilation.update_after_compute(
-            self._run_results,
-            self._data_assimilation_parameters,
-            self._run_parameters,
-            )
-        
-        (self._values['channel_exit_water_x-section__volume_flow_rate'], 
-         self._values['channel_water_flow__speed'], 
-         self._values['channel_water__mean_depth'], 
-         self._values['lake_water~incoming__volume_flow_rate'], 
-         self._values['lake_water~outgoing__volume_flow_rate'], 
-         self._values['lake_surface__elevation'],
-        ) = create_output_dataframes(
-            self._run_results, 
-            self._nts, 
-            self._network._waterbody_df,
-            self._network.link_lake_crosswalk)
-        
-        self._time += self._time_step * self._nts
+        if self._model._time==0.0:
+            self._model.preprocess_static_vars(self._values) 
+            
+        self._model.run(self._values)
 
     def update_frac(self, time_frac):
         """Update model by a fraction of a time step.
@@ -610,42 +522,3 @@ class bmi_troute(Bmi):
         return cfg_list
 
 
-def create_output_dataframes(results, nts, waterbodies_df, link_lake_crosswalk):
-    
-    qvd_columns = pd.MultiIndex.from_product(
-        [range(int(nts)), ["q", "v", "d"]]
-    ).to_flat_index()
-    
-    flowveldepth = pd.concat(
-        [pd.DataFrame(r[1], index=r[0], columns=qvd_columns) for r in results], copy=False,
-    )
-    
-    # create waterbody dataframe for output to netcdf file
-    i_columns = pd.MultiIndex.from_product(
-        [range(int(nts)), ["i"]]
-    ).to_flat_index()
-    
-    wbdy = pd.concat(
-        [pd.DataFrame(r[6], index=r[0], columns=i_columns) for r in results],
-        copy=False,
-    )
-    
-    wbdy_id_list = waterbodies_df.index.values.tolist()
-
-    i_lakeout_df = wbdy.loc[wbdy_id_list].iloc[:,-1]
-    q_lakeout_df = flowveldepth.loc[wbdy_id_list].iloc[:,-3]
-    d_lakeout_df = flowveldepth.loc[wbdy_id_list].iloc[:,-1]
-    # lakeout = pd.concat([i_df, q_df, d_df], axis=1)
-    
-    # replace waterbody lake_ids with outlet link ids
-    #TODO Update the following line to fit with HyFeatures. Do we need to replace IDs? Or replace
-    # waterbody_ids with the downstream segment?
-    #flowveldepth = _reindex_lake_to_link_id(flowveldepth, link_lake_crosswalk)
-    
-    q_channel_df = flowveldepth.iloc[:,-3]
-    v_channel_df = flowveldepth.iloc[:,-2]
-    d_channel_df = flowveldepth.iloc[:,-1]
-    
-    segment_ids = flowveldepth.index.values.tolist()
-
-    return q_channel_df, v_channel_df, d_channel_df, i_lakeout_df, q_lakeout_df, d_lakeout_df#, wbdy_id_list, 
