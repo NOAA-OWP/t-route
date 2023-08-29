fb9d375266bac8351fe8f1910bd50d82f842b2f2
Sean Horvath <sean.horvath@noaa.gov>
2023-08-29 14:43:47 +0000

    delete extra build_coastal_ncdf_dataframe() function

--- src/troute-network/troute/nhd_io.py
+++ src/troute-network/troute/nhd_io.py
@@ -1575,11 +1575,6 @@ def build_coastal_dataframe(coastal_boundary_elev):
     return coastal_df
 
 
-def build_coastal_ncdf_dataframe(coastal_ncdf):
-    with xr.open_dataset(coastal_ncdf) as ds:
-        coastal_ncdf_df = ds[["elev", "depth"]]
-        return coastal_ncdf_df.to_dataframe()
-
 def build_coastal_ncdf_dataframe(
                                 coastal_files, 
                                 coastal_boundary_domain,
