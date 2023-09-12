7958f9caa7268e4ae14d353b48ee619cdfbd2292
AminTorabi-NOAA <amin.torabi@noaa.gov>
2023-09-12 13:40:21 -0400

    nudged value added in return

--- src/troute-routing/troute/routing/fast_reach/mc_reach.pyx
+++ src/troute-routing/troute/routing/fast_reach/mc_reach.pyx
@@ -748,4 +748,32 @@ cpdef object compute_network_structured(
     #do the same for the upstream_array
     output_upstream = np.asarray(upstream_array[:,1:,:], dtype='float32')
     #return np.asarray(data_idx, dtype=np.intp), np.asarray(flowveldepth.base.reshape(flowveldepth.shape[0], -1), dtype='float32')
-    return np.asarray(data_idx, dtype=np.intp)[fill_index_mask], output.reshape(output.shape[0], -1)[fill_index_mask], 0, (np.asarray([data_idx[usgs_position_i] for usgs_position_i in usgs_positions]), np.asarray(lastobs_times), np.asarray(lastobs_values)), (usgs_idx, usgs_update_time-((timestep-1)*dt), usgs_prev_persisted_ouflow, usgs_prev_persistence_index, usgs_persistence_update_time-((timestep-1)*dt)), (usace_idx, usace_update_time-((timestep-1)*dt), usace_prev_persisted_ouflow, usace_prev_persistence_index, usace_persistence_update_time-((timestep-1)*dt)), output_upstream.reshape(output.shape[0], -1)[fill_index_mask], (rfc_idx, rfc_update_time-((timestep-1)*dt), rfc_timeseries_idx)
+    return (
+        np.asarray(data_idx, dtype=np.intp)[fill_index_mask], 
+        output.reshape(output.shape[0], -1)[fill_index_mask], 
+        0, 
+        (
+            np.asarray([data_idx[usgs_position_i] for usgs_position_i in usgs_positions]), 
+            np.asarray(lastobs_times), 
+            np.asarray(lastobs_values)
+        ), 
+        (
+            usgs_idx, 
+            usgs_update_time-((timestep-1)*dt), 
+            usgs_prev_persisted_ouflow, 
+            usgs_prev_persistence_index, 
+            usgs_persistence_update_time-((timestep-1)*dt)
+        ), 
+        (
+            usace_idx, usace_update_time-((timestep-1)*dt), 
+            usace_prev_persisted_ouflow, 
+            usace_prev_persistence_index, 
+            usace_persistence_update_time-((timestep-1)*dt)
+        ), 
+        output_upstream.reshape(output.shape[0], -1)[fill_index_mask], 
+        (
+            rfc_idx, rfc_update_time-((timestep-1)*dt), 
+            rfc_timeseries_idx
+        ),
+        np.asarray(nudge)
+    )
