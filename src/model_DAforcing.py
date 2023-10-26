fc1c1cb01b657ea872b7afce8b0e811fcae778e5
JurgenZach-NOAA <jzach@lynker.com>
2023-10-26 15:42:07 -0700

    BMI compliant arrays, implemented for USGS and USACE t-domain

--- src/model_DAforcing.py
+++ src/model_DAforcing.py
@@ -30,13 +30,18 @@ class DAforcing_model():
             self._forcing_parameters = forcing_parameters
             self._data_assimilation_parameters = data_assimilation_parameters
 
+            print(data_assimilation_parameters)
+
             #############################
             # Read DA files:
             #############################
             nudging = data_assimilation_parameters.get('streamflow_da', {}).get('streamflow_nudging', False)
+            
             usgs_persistence = data_assimilation_parameters.get('reservoir_da', {}).get('reservoir_persistence_da', {}).get('reservoir_persistence_usgs', False)
             usace_persistence = data_assimilation_parameters.get('reservoir_da', {}).get('reservoir_persistence_da', {}).get('reservoir_persistence_usace', False)
             rfc = data_assimilation_parameters.get('reservoir_da', {}).get('reservoir_rfc_da', {}).get('reservoir_rfc_forecasts', False)
+
+            #usace_persistence = False
 
             qc_threshold = data_assimilation_parameters.get('qc_threshold')
             cpu_pool = compute_parameters.get('cpu_pool')
@@ -84,7 +89,9 @@ class DAforcing_model():
                                                                     900, #15 minutes
                                                                     cpu_pool,)
 
-            # USACE Observations
+            #import pdb; pdb.set_trace()
+
+            # USACE Observations        
             if usace_persistence:
                 usace_timeslice_path = str(data_assimilation_parameters.get('usace_timeslices_folder'))
                 self._reservoir_usace_df = _read_timeslice_files(usace_timeslice_path, 
@@ -92,7 +99,7 @@ class DAforcing_model():
                                                                  qc_threshold,
                                                                  900, #15 minutes
                                                                  cpu_pool,)
-            
+
             # Produce list of datetimes to search for timeseries files
             rfc_parameters = data_assimilation_parameters.get('reservoir_da', {}).get('reservoir_rfc_da', {})
             lookback_hrs = rfc_parameters.get('reservoir_rfc_forecasts_lookback_hours')
@@ -117,9 +124,307 @@ class DAforcing_model():
             if lastobs_file:
                 self._lastobs_df = _read_lastobs_file(lastobs_file)
 
+            # to do: flatten the following arrays:
+            #   Confirm that there is no usace dataframe (only a reservoir usace dataframe)? 
+            #   self._reservoir_usace_df
+
+            #import pdb; pdb.set_trace()
+
+            # save time reference: start_datetime
+            self.dateNull = start_datetime
+
+            # read in metadata for BMI compliant arrays:
+
+            # USGS Observations
+            if nudging or usgs_persistence:
+
+                # 
+                # USGS dataframe: 
+                # 
+                # Dates related:
+                # - datesSecondsArray_usgs: "column names" of usgs dataframe: dates
+                #        in seconds relative to start_datetime
+                # - nDates_usgs: number of "dates" columns
+                # 
+                # Station ID related:
+                # - stationArray_usgs: "row names" of usgs dataframe: all station
+                #        names decoded into arrays of one ASCII code per string,
+                #        attached together
+                # - stationStringLengthArray_usgs: array of lengths of station IDs,
+                #        in case of mixed arrays (not all station IDs have equal lengths)
+                # - nStations_usgs: number of stations
+                #
+                ( _datesSecondsArray_usgs, _nDates_usgs, _stationArray_usgs, \
+                    _stationStringLengthArray_usgs, _nStations_usgs) \
+                    = _time_stations_from_df(self._usgs_df,start_datetime)
+                # save metadata in class instances
+                self.datesSecondsArray_usgs = _datesSecondsArray_usgs
+                self.nDates_usgs = _nDates_usgs
+                self.stationArray_usgs = _stationArray_usgs
+                self.stationStringLengthArray_usgs = _stationStringLengthArray_usgs
+                self.nStations_usgs = _nStations_usgs
+                # flatten the actual USGS datafrane into a numpy ndarray
+                _usgsArray = _flatten_array(self._usgs_df)
+                # ... and save it with the class instance
+                self.usgsArray = _usgsArray
+         
+                #import pdb; pdb.set_trace()
+
+                # 
+                # Reservoir USGS and USACE dataframes: Array structures of 
+                # _reservoir_usgs_df and _reservoir_usace_df are
+                # fundamentally the same as usgs_df: input/output analogous.
+                # 
+                # In the following comments, usgs shall also represent usace
+                #
+                # Dates related:
+                # - datesSecondsArray_reservoir_usgs
+                # - nDates_usgs
+                # 
+                # Station ID related:
+                # - stationArray_usgs 
+                # - stationStringLengthArray_usgs 
+                # - nStations_usgs 
+                #
+                ( _datesSecondsArray_reservoir_usgs, _nDates_reservoir_usgs, \
+                     _stationArray_reservoir_usgs, _stationStringLengthArray_reservoir_usgs, \
+                    _nStations_reservoir_usgs) \
+                    = _time_stations_from_df(self._reservoir_usgs_df,start_datetime)
+                # save metadata in class instances
+                self.datesSecondsArray_reservoir_usgs = _datesSecondsArray_reservoir_usgs
+                self.nDates_reservoir_usgs = _nDates_reservoir_usgs
+                self.stationArray_reservoir_usgs = _stationArray_reservoir_usgs
+                self.stationStringLengthArray_reservoir_usgs = _stationStringLengthArray_reservoir_usgs
+                self.nStations_reservoir_usgs = _nStations_reservoir_usgs
+                # flatten the actual USGS datafrane into a numpy ndarray
+                _reservoirUsgsArray = _flatten_array(self._reservoir_usgs_df)
+                # ... and save it with the class instance
+                self.reservoirUsgsArray = _reservoirUsgsArray            
+
+            # USACE Observations        
+            if usace_persistence:
+
+                # see detailed comments in USGS branch
+                ( _datesSecondsArray_reservoir_usace, _nDates_reservoir_usace, \
+                    _stationArray_reservoir_usace, _stationStringLengthArray_reservoir_usace, \
+                    _nStations_reservoir_usace) \
+                    = _time_stations_from_df(self._reservoir_usace_df,start_datetime)
+                # save metadata in class instances
+                self.datesSecondsArray_reservoir_usace = _datesSecondsArray_reservoir_usace
+                self.nDates_reservoir_usace = _nDates_reservoir_usace
+                self.stationArray_reservoir_usace = _stationArray_reservoir_usace
+                self.stationStringLengthArray_reservoir_usace = _stationStringLengthArray_reservoir_usace
+                self.nStations_reservoir_usace = _nStations_reservoir_usace
+                # flatten the actual USACE datafrane into a numpy ndarray
+                _reservoirUsaceArray = _flatten_array(self._reservoir_usace_df)
+                # ... and save it with the class instance
+                self.reservoirUsaceArray = _reservoirUsaceArray  
+
+                #import pdb; pdb.set_trace()
+
+            testRevert = True
+            # The following code segments revert the array troute -> bmi-flattened
+            # array conversions, and are placed here only temporarily for
+            # verification purposes, to be moved to DataAssimilation eventually.
+            # If "reverse" code is not removed, it can be temporarily disabled
+            # by setting the testRevert flag to False
+            if (testRevert):
+
+                # USGS dataframe
+                
+                #import pdb; pdb.set_trace()
+                # Unflatten the arrays
+                df_raw_usgs = _unflatten_array(self.usgsArray,self.nDates_usgs,\
+                                          self.nStations_usgs)
+                #import pdb; pdb.set_trace()
+                # Decode time/date axis
+                timeAxisName = 'time'
+                freqString = '5T'
+                df_withDates_usgs = _time_retrieve_from_arrays(df_raw_usgs, self.dateNull, \
+                            self.datesSecondsArray_usgs, timeAxisName, freqString)
+                #import pdb; pdb.set_trace()
+                # Decode station ID axis
+                stationAxisName = 'stationId'
+                df_withStationsAndDates_usgs = _stations_retrieve_from_arrays(df_withDates_usgs, \
+                    self.stationArray_usgs, self.stationStringLengthArray_usgs, \
+                    stationAxisName)
+
+                # Reservoir USGS dataframe
+
+                #import pdb; pdb.set_trace()
+                # Unflatten the arrays
+                df_raw_reservoirUsgs = _unflatten_array(self.reservoirUsgsArray,self.nDates_reservoir_usgs,\
+                                          self.nStations_reservoir_usgs)
+                #import pdb; pdb.set_trace()
+                # Decode time/date axis
+                timeAxisName = 'time'
+                freqString = '15T'
+                df_withDates_reservoirUsgs = _time_retrieve_from_arrays(df_raw_reservoirUsgs, self.dateNull, \
+                            self.datesSecondsArray_reservoir_usgs, timeAxisName, freqString)
+                #import pdb; pdb.set_trace()
+                # Decode station ID axis
+                stationAxisName = 'stationId'
+                df_withStationsAndDates_reservoirUsgs = _stations_retrieve_from_arrays(df_withDates_reservoirUsgs, \
+                    self.stationArray_reservoir_usgs, self.stationStringLengthArray_reservoir_usgs, \
+                    stationAxisName)
+                
+                # Reservoir USACE dataframe
+
+                #import pdb; pdb.set_trace()
+                # Unflatten the arrays
+                df_raw_reservoirUsace = _unflatten_array(self.reservoirUsaceArray,self.nDates_reservoir_usace,\
+                                          self.nStations_reservoir_usace)
+                #import pdb; pdb.set_trace()
+                # Decode time/date axis
+                timeAxisName = 'time'
+                freqString = '15T'
+                df_withDates_reservoirUsace = _time_retrieve_from_arrays(df_raw_reservoirUsace, self.dateNull, \
+                            self.datesSecondsArray_reservoir_usace, timeAxisName, freqString)
+                #import pdb; pdb.set_trace()
+                # Decode station ID axis
+                stationAxisName = 'stationId'
+                df_withStationsAndDates_reservoirUsace = _stations_retrieve_from_arrays(df_withDates_reservoirUsace, \
+                    self.stationArray_reservoir_usace, self.stationStringLengthArray_reservoir_usace, \
+                    stationAxisName)                
+
+                import pdb; pdb.set_trace()
+
         else:
             raise(RuntimeError("No config file provided."))
-            
+
+
+def _time_stations_from_df(dataFrame, timeBase):
+
+    # get columns (date-time), extract as list at first
+    datesList = (dataFrame.columns).tolist()
+    # then subtract timebase
+    datesListSeconds = [int((d-timeBase).total_seconds()) for d in datesList]
+    nDates = len(datesListSeconds)
+    datesSecondsArray = np.array(datesListSeconds)
+
+    # get station IDs, as list
+    stations = (dataFrame).index.tolist()
+    nStations = len(stations)
+
+    # build station array (one giant 1D ndarray with ASCII encodings
+    # concatenated together), aided by an array of how many strings each
+    # station ID consists of - completely USGS, USACE, etc agnostic 
+    #
+    # define array defining string length for each station ID
+    stationStringLengthArray = np.empty(nStations, dtype=np.int64)
+    stationStringLengthArray[0] = len(stations[0])
+
+    # start big array with ID for first station
+    stationArray = np.empty(stationStringLengthArray[0], dtype=np.int64)
+
+    # init some aux. variables
+    firstIter = True
+    stationIndex = 0
+    # iterate through all stations
+    for station in stations:
+        # capture string length of present station
+        stationStringLengthArray[stationIndex] = len(station)
+        stationIndex += 1        
+        # convert string to ASCII code
+        charArray = np.array(station, dtype='c')
+        addArray = np.array([ord(ch) for ch in charArray])        
+        # build up big array
+        if (firstIter):
+            stationArray = addArray
+            firstIter = False
+        else:
+            stationArray = np.concatenate([stationArray, addArray])
+
+    return datesSecondsArray, nDates, stationArray, stationStringLengthArray, nStations
+
+
+def _flatten_array(dataFrame):
+
+    # convert to numpy array first
+    array1 = dataFrame.to_numpy(copy=True, dtype=np.float16)
+    # flatten it
+    array2 = array1.reshape(-1)
+
+    return array2
+
+
+def _unflatten_array(array_1D,nx,ny):
+
+    # reshape numpy array
+    array_2D = array_1D.reshape(ny,nx)
+
+    # convert to data frame
+    df_raw = pd.DataFrame(array_2D)
+
+    return df_raw
+
+
+def _time_retrieve_from_arrays(dataFrame, timeBase, datesSecondsArrays, \
+                               timeAxisName, freqString):
+
+    # convert array to list at first
+    datesList = (datesSecondsArrays).tolist()
+
+    # add timebase back in, and convert to datetime format
+    datesList = [(timedelta(seconds=d)+timeBase) for d in datesList]
+
+    # convert to DatetimeIndex
+    dateTimeIndex = pd.DatetimeIndex(datesList, name=timeAxisName, freq=freqString)
+
+    # transpose dataframe
+    dataFrameTranspose = dataFrame.T
+    # add time axis as index on transposed dataframe (time is in rows)
+    dataFrameTranspose = dataFrameTranspose.set_index(dateTimeIndex)
+    # revert to original (time is in columns)
+    dataFrameWithTime = dataFrameTranspose.T
+
+    return dataFrameWithTime
+
+
+def _stations_retrieve_from_arrays(dataFrame, stationsArray, stationStringLengthArray, \
+                               stationAxisName):
+
+    # first create station outputs as list
+    stationsList = []
+
+    # mark place in global stationsArray
+    i=0
+
+    for stringLength in stationStringLengthArray:
+
+        # extract small array with ASCII codes of one station ID
+        stationID_asArray = stationsArray[i:i+stringLength]
+        # increment global pointer along array
+        i=i+stringLength
+        # convert to list of characters
+        firstRun = True
+        for asciiCode in stationID_asArray:
+            # decode Ascii back to character and stick it to the end of the list
+            chrAdd = chr(asciiCode)
+            if firstRun == True:
+                stationID_asString = chrAdd
+                firstRun = False
+            else:
+                stationID_asString += chrAdd
+
+        #import pdb; pdb.set_trace()
+
+        # done with one station ID
+        stationsList.append(stationID_asString)
+
+    # construct index    
+    index = pd.Index(stationsList, name=stationAxisName, dtype=object)
+
+    dataFrame.index = (index)
+
+    #import pdb; pdb.set_trace()
+
+    return dataFrame
+
+
+
+        
+
 
 # Utility functions -------
 def _read_config_file(custom_input_file):
@@ -165,10 +470,20 @@ def _read_timeslice_files(filepath,
     observation_df = pd.DataFrame()
     for d in dates:
         f = glob.glob(filepath + '/' + d + '*')
+
+        print('date ',d)
+        print('file ',filepath + '/' + d + '*')
+
         if f:
             temp_df = xr.open_dataset(f[0])[['stationId','time','discharge','discharge_quality']].to_dataframe()
             observation_df = pd.concat([observation_df, temp_df])
     
+
+    print('Dates ',dates)
+    print('Observation station ID ',observation_df)
+
+    #import pdb; pdb.set_trace()
+
     observation_df['stationId'] = observation_df['stationId'].str.decode('utf-8').str.strip()
     observation_df['time'] = observation_df['time'].str.decode('utf-8')
     observation_df['discharge_quality'] = observation_df['discharge_quality']/100
@@ -190,6 +505,8 @@ def _read_timeslice_files(filepath,
     # specify resampling frequency 
     frequency = str(int(frequency_secs/60))+"min"    
     
+    print('Enter interpolate and resample')
+
     # interpolate and resample frequency
     buffer_df = observation_df_T.resample(frequency).asfreq()
     with Parallel(n_jobs=cpu_pool) as parallel:
