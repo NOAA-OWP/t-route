0f376b3329a400aec5c8e38d916823667e9a70f8
AminTorabi-NOAA <amin.torabi@noaa.gov>
2023-12-28 13:30:52 -0500

    update for when there is no lake in the gpkg

--- src/troute-network/troute/AbstractNetwork.py
+++ src/troute-network/troute/AbstractNetwork.py
@@ -622,11 +622,13 @@ class AbstractNetwork(ABC):
                     waterbodies_initial_states_df["index"] = range(
                         len(waterbodies_initial_states_df)
                     )
-            
-            self._waterbody_df = pd.merge(
-                self.waterbody_dataframe, waterbodies_initial_states_df, on=index_id
-            )
-
+            import pdb;pdb.set_trace()
+            if len(self.waterbody_dataframe) > 0:
+                self._waterbody_df = pd.merge(
+                    self.waterbody_dataframe, waterbodies_initial_states_df, on=index_id
+                )
+            else:
+                self._waterbody_df = pd.DataFrame()
             LOG.debug(
                 "waterbody initial states complete in %s seconds."\
                 % (time.time() - start_time))
