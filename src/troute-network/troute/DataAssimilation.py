aa7f3d68e7a43d609c6b3a9b7052bd2e2a2532aa
JurgenZach-NOAA <jurgen.zach@noaa.gov>
2024-02-23 14:17:26 -0700

    Removed debug line

--- src/troute-network/troute/DataAssimilation.py
+++ src/troute-network/troute/DataAssimilation.py
@@ -138,7 +138,6 @@ class NudgingDA(AbstractDA):
                 #usgs_df = usgs_df.join(network.link_gage_df.reset_index().set_index('gages'),how='inner').set_index('link').sort_index()
                 usgs_df = network.link_gage_df.reset_index().set_index('gages').join(usgs_df).set_index('link').sort_index()
 
-                import pdb; pdb.set_trace()
                 # only run if there are lakes
                 if (len(network._waterbody_types_df) > 0):
                     self._usgs_df = _reindex_link_to_lake_id(usgs_df, network.link_lake_crosswalk)
