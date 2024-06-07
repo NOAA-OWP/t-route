21f9441b94248a782fcaa86dda3490bd6f3295fa
kumdonoaa <dongha_unix@DESKTOP-TB1SOAQ>
2024-06-07 09:43:34 -0500

    fix for diffusive to run with synthetic and topo on NHD

--- src/troute-network/troute/AbstractNetwork.py
+++ src/troute-network/troute/AbstractNetwork.py
@@ -474,10 +474,10 @@ class AbstractNetwork(ABC):
         routing_type = [run_hybrid, use_topobathy, run_refactored]
 
         _routing_scheme_map = {
-            MCOnly: [False, False, False],
-            MCwithDiffusive: [True, False, False],
-            MCwithDiffusiveNatlXSectionNonRefactored: [True, True, False],
-            MCwithDiffusiveNatlXSectionRefactored: [True, True, True],
+            MCOnly: [[False, False, False]],
+            MCwithDiffusive: [[True, False, False],[True, True, False]],
+            MCwithDiffusiveNatlXSectionNonRefactored: [[True, True, False]],
+            MCwithDiffusiveNatlXSectionRefactored: [[True, True, True]],
             }
         
         # Default to MCOnly routing
@@ -485,9 +485,9 @@ class AbstractNetwork(ABC):
 
         # Check user input to determine the routing scheme
         for key, value in _routing_scheme_map.items():
-            if value==routing_type:
+            if routing_type in value:
                 routing_scheme = key
-        
+
         routing = routing_scheme(self.hybrid_parameters)
 
         (
